b33e47c362986cc1e3309779feaba34782a24ec0
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-04-26 11:33:42 -0400

    Update change_desktop.py

--- Animate wallpaper/change_desktop.py
+++ Animate wallpaper/change_desktop.py
@@ -12,12 +12,6 @@ for filename in glob.glob("C:/Users/WilganZMT/Documents/Coding/Python/Animation 
 
 len(image_list)   #60 images
 
-pics={}        #empty dictionary to fill later
-    
-for img in image_list:               #This automatically saves dictionary keys as whatever you want. pics['2'] gives the 3rd value.
-    for i in range(len(image_list)):
-        pics[f"{i}"] = "C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Water ripple/frame_" + str(img) + "_delay-0.03s.png"  
-
 def animate():
     while True:            #The while loop keeps the animation running constantly.
         for n in range(len(image_list)):
