a85151e6c2acffc9c95acf47d1c6d5f038df4be5
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-04-27 11:06:14 -0400

    Update change_desktop.py

--- Animate wallpaper/change_desktop.py
+++ Animate wallpaper/change_desktop.py
@@ -5,17 +5,17 @@ from PIL import Image
 from datetime import datetime
 
 water_list = []
-for filename in glob.glob("C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Water ripple/*.png"):
+for filename in glob.glob("pic folder 1 path/*.png"):  #change to your path
     im = Image.open(filename)
     water_list.append(im)
 
 lake_list = []
-for filename in glob.glob("C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Lake night/*.png"):
+for filename in glob.glob("pic folder 2 path/*.png"):
     zo = Image.open(filename)
     lake_list.append(zo)
 
 universe_list = []
-for filename in glob.glob("C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Universe/*.png"):
+for filename in glob.glob("pic folder 3 path/*.png"):
     tq = Image.open(filename)
     universe_list.append(tq)
 
@@ -24,16 +24,16 @@ def animate():
         while 7 <= datetime.now().hour < 15:
             for n in range(len(water_list)):
                 time.sleep(0.05)
-                ctypes.windll.user32.SystemParametersInfoW(20,0, "C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Water ripple/frame_" + str(n) + "_delay-0.03s.png", 0)
+                ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 1 path" + str(n) + "_delay-0.03s.png", 0)
                 continue
         while 15 <= datetime.now().hour < 20:
             for nm in range(len(universe_list)):
                 time.sleep(0.05)
-                ctypes.windll.user32.SystemParametersInfoW(20,0, "C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Universe/frame_" + str(nm) + "_delay-0.1s.png", 0)
+                ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 2 path" + str(nm) + "_delay-0.1s.png", 0)
                 continue
         while 20 <= datetime.now().hour <= 24 or 0 <= datetime.now().hour < 7 :
             for nm in range(len(lake_list)):
                 time.sleep(0.05)
-                ctypes.windll.user32.SystemParametersInfoW(20,0, "C:/Users/WilganZMT/Documents/Coding/Python/Animation Background/pics/Lake night/frame_" + str(nm) + "_delay-0.06s.png", 0)
+                ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 3 path" + str(nm) + "_delay-0.06s.png", 0)
                 continue
             continue
