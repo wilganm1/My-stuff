31c88ac89c905d8670f63764d833a915a7ba000f
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-05-01 09:52:59 -0400

    Update change_desktop.py

--- Animate wallpaper/change_desktop.py
+++ Animate wallpaper/change_desktop.py
@@ -19,21 +19,21 @@ for filename in glob.glob("pic folder 3 path/*.png"):
     tq = Image.open(filename)
     universe_list.append(tq)
 
-def animate():
-    while True:
-        while 7 <= datetime.now().hour < 15:
-            for n in range(len(water_list)):
-                time.sleep(0.05)
-                ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 1 path" + str(n) + "_delay-0.03s.png", 0)
-                continue
-        while 15 <= datetime.now().hour < 20:
-            for nm in range(len(universe_list)):
-                time.sleep(0.05)
-                ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 2 path" + str(nm) + "_delay-0.1s.png", 0)
-                continue
-        while 20 <= datetime.now().hour <= 24 or 0 <= datetime.now().hour < 7 :
-            for nm in range(len(lake_list)):
-                time.sleep(0.05)
-                ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 3 path" + str(nm) + "_delay-0.06s.png", 0)
-                continue
+#You can use a function command, but I'm going to put this into an exe file, so I exclude it.
+while True:
+    while 7 <= datetime.now().hour < 15:
+        for n in range(len(water_list)):
+            time.sleep(0.05)
+            ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 1 path" + str(n) + "_delay-0.03s.png", 0)
             continue
+    while 15 <= datetime.now().hour < 20:
+        for nm in range(len(universe_list)):
+            time.sleep(0.05)
+            ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 2 path" + str(nm) + "_delay-0.1s.png", 0)
+            continue
+    while 20 <= datetime.now().hour <= 24 or 0 <= datetime.now().hour < 7 :
+        for nm in range(len(lake_list)):
+            time.sleep(0.05)
+            ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 3 path" + str(nm) + "_delay-0.06s.png", 0)
+            continue
+        continue
