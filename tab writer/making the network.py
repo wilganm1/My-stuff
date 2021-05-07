f53df0a87cbac3075d28a6108c6b2869288d10aa
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-05-07 19:22:15 -0400

    Update making the network.py

--- tab writer/making the network.py
+++ tab writer/making the network.py
@@ -31,94 +31,20 @@ b
 print("Arrays in b: " + str(len(b)))
 n = len(b)
 n
-
 b[3550] #last array
-
-#add the array values together
-b[0][0] + b[0][1]
-b[1][0] + b[1][1]
-
-add_values = []
-for i in b:
-    toq = i[0] + i[1]
-    add_values.append(toq)
-add_values
-
-"""
-Okay so...I couldn't get the sigmoid function to
-work because I can't put in arrays. So what I've
-decided to do is to combine the two values in 
-each [] in the b matrix together, then 
-multiply those by a random weight, and plug those
-values into the sigmoid function 
-"""
-
-"""
-Lets try to loop all the arrays as inputs for 
-the first layer of the network. Weights will 
-be random using random.uniform()
-"""
-
-inputs = [b[i][0:] for i in range(0, n-1)]
-inputs[0:2]
-
 weights = []
-for ii in range(0, 3550):
-    ii = (random.uniform(-1.00, 1.00))
-    weights.append(ii)
-
-bias1 = np.array([1])
-
-"""
-Okay, I have the inputs and the weights to go with 
-it. Now its time to see if I can multiply the 
-arrays with weights.
-"""
-
-layer1_outputs = []
-for i in range(len(inputs)):
-    layer1_outputs.append(inputs[i] * weights[i])
+for x in range(0,3551):
+    weights.append(random.uniform(-1,1))
     
-layer1_outputs.append(np.array([1]))
+#Now to get the ouput from the dot product. Can't actually run the dot method, so we need to loop it.
+output = [0,0]
+for qu in range(0,3551):
+    output += (b[qu] * weights[qu])    #This is the dot product for this first layer.
 
-layer1_outputs
-
-"""
-I might only need one node for input, no hidden
-layers, and one ouput node for one single note.
-
-so y = [1] if its a 60 note.
-"""
-
-#theta = weights. 
-#x = inputs
-
-weights = np.asarray(weights)
-
+print(output)
+output + 1
 
 """
-May need to combine the values in each array so I
-can run sigmoid function
-"""
-
-weights
-add_values
-
-weights[0] *add_values[0]
-weights[0:2]*add_values[0:2]
-
-#hθ(x) = 1/(1+e^(-(θ^T)*x)) 
-
-bie = []
-for i in range(len(weights)):
-    bie.append(weights[i] *add_values[i])
-print(bie)
-
-   
-eib = np.asarray(bie)
-eib[0]
-
-
 def sigmoid():
     uqo = (1 / (1 + (math.e**-(eib))))
     print(uqo)
@@ -128,12 +54,4 @@ def sigmoid():
         if i >= 0.5:
             total+= 1
     print(total)
-    
-
-3550/1791
-
-1791/3550
-def sig():
-      answer = (1 / (1 + np.exp(-eib)))
-      print(answer)
-
+"""   
