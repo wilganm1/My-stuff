9e5ee4cfaab4c55b6ec23009f44e187419dba55f
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-05-08 21:08:54 -0400

    Update change_desktop.py

--- Animate wallpaper/change_desktop.py
+++ Animate wallpaper/change_desktop.py
@@ -4,36 +4,26 @@ import time
 from PIL import Image
 from datetime import datetime
 
-water_list = []
+water_list = []    #list name
 for filename in glob.glob("pic folder 1 path/*.png"):  #change to your path
     im = Image.open(filename)
     water_list.append(im)
 
-lake_list = []
-for filename in glob.glob("pic folder 2 path/*.png"):
-    zo = Image.open(filename)
-    lake_list.append(zo)
-
-universe_list = []
-for filename in glob.glob("pic folder 3 path/*.png"):
-    tq = Image.open(filename)
-    universe_list.append(tq)
-
 #You can use a function command, but I'm going to put this into an exe file, so I exclude it.
 while True:
-    while 7 <= datetime.now().hour < 15:
+    while 7 <= datetime.now().hour < 15:   #Sets when it happens according to time of day on your system.
         for n in range(len(water_list)):
             time.sleep(0.05)
             ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 1 path" + str(n) + "_delay-0.03s.png", 0)
             continue
-    while 15 <= datetime.now().hour < 20:
-        for nm in range(len(universe_list)):
-            time.sleep(0.05)
-            ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 2 path" + str(nm) + "_delay-0.1s.png", 0)
-            continue
-    while 20 <= datetime.now().hour <= 24 or 0 <= datetime.now().hour < 7 :
-        for nm in range(len(lake_list)):
-            time.sleep(0.05)
-            ctypes.windll.user32.SystemParametersInfoW(20,0, "pic folder 3 path" + str(nm) + "_delay-0.06s.png", 0)
-            continue
-        continue
+"""           
+
+
+
+
+Add more here
+
+
+
+
+"""
