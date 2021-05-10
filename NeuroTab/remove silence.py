3dae6f10c7b0f2abc08a83bad4a78d92a26025a2
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-05-09 22:13:36 -0400

    Update remove silence.py

--- NeuroTab/remove silence.py
+++ NeuroTab/remove silence.py
@@ -12,9 +12,7 @@ Then just reshape it back to its normal shape.
 import numpy as np
 from scipy.io import wavfile
 from playsound import playsound
-
-#Test archive the numpy for the test file.
-#6th string open note
+#Test using the test file.
 playsound('testing.wav')
 a = wavfile.read('testing.wav')
 b = np.array(a[1], dtype = float)
@@ -22,17 +20,15 @@ b
 print(b)
 
 #Now to create a new list and append every value.
-
-
-new_b = []
+wav_array = []
 for array in b:
     for value in array:
-        new_b.append(float(value))
-len(new_b)
-new_b = [item for item in new_b if abs(item) > 0.00009]
-new_b.append(200)
-len(new_b)
+        wav_array.append(float(value))
+len(wav_array)
+wav_array = [item for item in wav_array if abs(item) > 0.00009]
+wav_array.append(200)
+len(wav_array)
 7094/2
-new_b = np.reshape(new_b, (3547, 2))
-new_b
-print(new_b)
+wav_array = np.reshape(wav_array, (3547, 2))
+wav_array
+print(wav_array)
