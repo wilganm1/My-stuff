c506e40d09c66d3729aa96ccd1684253dbf061dd
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-01 09:20:33 -0400

    Updated scales and stuff
    I added in extra scales to pick from, and left comments on what everything means so people can understand every step.

--- Guitar scale maker/combined.py
+++ Guitar scale maker/combined.py
@@ -1,35 +1,35 @@
 from tkinter import *
 
 root = Tk()
-''' Make frame 1 with all the buttons '''
+root.title("Scale Maker")
+
+''' Make frame 1 with all the dropdown menus '''
 frame1 = Frame(master = root, width = 200, height = 100)
 
 #Tuning choices
 tuning_var = StringVar(root)
-
-tuning_choices = ['Standard', 'Drop D', 'D Standard', 'Drop C', 'C Standard']
 tuning_var.set('') # set the default option
 
+tuning_choices = ['Standard', 'Drop D', 'D Standard', 'Drop C', 'C Standard']
 tuningMenu = OptionMenu(frame1, tuning_var, *tuning_choices)
 Label(frame1, text="Select tuning").grid(row = 1, column = 1)
 tuningMenu.grid(row=2, column=1)
 
 #Root note choices
 root_note_var = StringVar(root)
-
-root_note_choices = ["A","A#","B","C", "C#","D","D#","E","F","F#","G","G#"]
 root_note_var.set('')
 
+root_note_choices = ["A","A#","B","C", "C#","D","D#","E","F","F#","G","G#"]
 rootNoteMenu= OptionMenu(frame1, root_note_var, *root_note_choices)
 Label(frame1, text="Select root note").grid(row=1, column = 2)
 rootNoteMenu.grid(row=2, column=2)
 
 #Scale choices
 scale_var = StringVar(root)
-
-scale_choices = ["Major", "Minor", "Phrygian", 'Lydian', "Mixolydian", 'Dorian', "Locrian"]
 scale_var.set('')
 
+scale_choices = ["Major", "Minor", "Phrygian", 'Lydian', "Mixolydian", 'Dorian', "Locrian",
+                 'Harmonic Minor', 'Melodic Minor', 'Major Pentatonic', 'Minor Pentatonic']
 scaleMenu = OptionMenu(frame1, scale_var, *scale_choices)
 Label(frame1, text="Select scale").grid(row=1, column = 3)
 scaleMenu.grid(row=2, column=3)
@@ -48,7 +48,7 @@ label2.pack()
   
 frame2.pack()
 
-''' Make frame 3 with the grid '''
+''' Make frame 3 with the grid for the frets'''
 frame3 = Frame(master = root)
 
 """ This function will show all the correct frets based on the chosen scale for each string in each tuning """
@@ -64,25 +64,34 @@ def ioq():
             )
             frame.grid(row=i, column=j, padx=2, pady=2)
             label = Label(master=frame)
-            notes = ["A","A#","B","C", "C#","D","D#","E","F","F#","G","G#"]
-            root = root_note_var.get()
+            notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+            root = root_note_var.get()  #This is will be whatever you pick from the dropdown
             r = 0 #The index of the note we pick that starts a scale
             w = 2 #W & h are called "steps", or how many frets away one note is from another
-            h  =1 #A whole-step, w, is 2 frets, and a half-step, h, is one fret. 
+            h = 1 #A whole-step, w, is 2 frets, and a half-step, h, is one fret. 
+            wh= 3 #A whole-half step. 3 frets away. 
             
+            
+            ''' The lists in this dictionary MUST ONLY have 7 letters including r.
+                            These are the formulas for each scale. '''
             scales = {'Major' : [r,w,w,h,w,w,w],
               'Minor' : [r,w,h,w,w,h,w],
               'Phrygian' : [r,h,w,w,w,h,w],
               'Lydian' : [r,w,w,w,h,w,w],
               'Mixolydian' : [r,w,w,h,w,w,h],
-              'Dorian' : [r,w,h,w,w,w,h],
-              'Locrian' : [r,h,w,w,h,w,w]}
+              'Dorian' : [r,w,h,w,w,w,h],   
+              'Locrian' : [r,h,w,w,h,w,w],
+              'Harmonic Minor' : [r,w,h,w,w,h,wh],
+              'Melodic Minor' : [r,w,h,w,w,w,w],
+              'Major Pentatonic' : [r,w,w,wh,w],
+              'Minor Pentatonic' : [r,wh,w,w,wh]}
+            
             scale_type = scale_var.get()
             note_list = []   #Empty list that will become our indices of notes
             scale_index = 0   #Starting index so the root note will be first
             scale = scales[scale_type] #Formula for the scale. r must be included
             for akj in scale:
-                scale_index += akj   #computes the indices from the values r, w, and h
+                scale_index += akj   #computes the indices from the values r, w, h, & wh
                 note_list.append(scale_index)  #Adds in index values. [0,...,12] 
                 zo = notes.index(root)   #Accesses index of chosen root note
                 ad = notes[zo:]     #Takes all notes from root index to the end
@@ -95,17 +104,18 @@ def ioq():
                 for hhq in note_list:     #loops through the numbers in scale. Those number are index values
                     this_scale.append(notes[hhq])   #adds in letters from notes variable that correspond to scale indices
             
-            tunings = {'Standard' : ["E","A","D","G","B","E"],  #Dictionary must be before input
+            '''  This is the tuning dictionary. Add in more if you'd like. The letters are the open notes. '''
+            tunings = {'Standard' : ["E","A","D","G","B","E"],  
                        'Drop D' : ["D","A","D","G","B","E"],
                        'D Standard' : ['D','G','C','F','A','D'],
                        'Drop C' : ['C','G','C','F','A','D'],
                        'C Standard' : ["C","F","A#","D#","G","C"]}
             
-            numbers = [0,1,2,3,4,5,6,7,8,9,10,11]
-            select = tuning_var.get()
-            results = []
-            for open_note in reversed(tunings[select]):
-                zo = notes.index(open_note)   
+            numbers = [0,1,2,3,4,5,6,7,8,9,10,11]    #this is a reference for the list of frets. We come back to it later
+            select = tuning_var.get()    #saves the tuning selection
+            results = []    #This will eventually have 6 lists of frets.
+            for open_note in reversed(tunings[select]):  #Reverse it so the 1st string is output first
+                zo = notes.index(open_note)   #Get indices of the open_note in notes. notes has changed based on the root note picked
                 ad = notes[zo:] 
                 ad.reverse()
                 del notes[zo:]    
@@ -114,17 +124,16 @@ def ioq():
                 a_list = []
                 for oq in this_scale:
                     if oq in notes:
-                        a_list.append(notes.index(oq))
-                results.append(a_list)
-            for yha in results:
-                yha.sort()    #Add in a few extra lines if you want to append the 12th fret.
-            for zzz in numbers:
-                for qot in results:
-                    if zzz not in qot:
-                        qot.insert(zzz, "-")
-            label.configure(text = results[i][j])
+                        a_list.append(notes.index(oq))  #Adds in correct fret for string
+                results.append(a_list)  #Adds in a_list as a nested list
+            for yha in results:  #Will go through every a_list for each string.
+                yha.sort()    #Sorts frets in ascending order. 
+            for zzz in numbers:       #loops through the numbers variable
+                for qot in results:     #loops through each a_list in results
+                    if zzz not in qot:     #loops through every fret in each a_list. checks if a number in numbers is not in the list
+                        qot.insert(zzz, "-") #inserts '-' into the place where a number is missing. Doing this allows us to display the list in tkinter
+            label.configure(text = results[i][j])    #mathces up the correct fret for each grid in tkinter.
             label.pack(padx=2, pady=2) 
             
 frame3.pack()
 root.mainloop()
-
