fbe5935c5656a6832c004ecdcc265fd9477f299f
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-06 18:15:57 -0400

    Update capitalize each word.py

--- misc./capitalize each word.py
+++ misc./capitalize each word.py
@@ -2,8 +2,8 @@
 
 variable = ['habt','aqnu','zjll','gioqng']  #change 'variable' to whatever you want. The words in the list are what you want to capitalize
 
-for word in range(len(variable)):  #Iterates through the words in the list by their index.
-    variable[word] = variable[word].capitalize()    #capitalize() automatically captializes the first letter for each string element. So do it for every index.
+for i in range(len(variable)):  #Iterates through the words in the list by their index.
+    variable[i] = variable[i].capitalize()    #capitalize() automatically captializes the first letter for each string element. So do it for every index.
     
 variable
     
