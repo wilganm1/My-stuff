d96578acceaa4c5d08b350470bcb6f8211d87bfe
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-11 16:56:50 -0400

    Update main.py

--- My AI/main.py
+++ My AI/main.py
@@ -16,28 +16,18 @@ answers/responses to my question/orders.
 
 """ Step 1: Speech recognition & text-to-speech
 Write function that will listen to my voice 
-and recognize speech. Then have it play back."""
+and recognize speech. Then have it respond."""
 
 import random
-import time
 import speech_recognition as sr
 import pyttsx3
 import os
 import wolframalpha
 import wikipedia
-import twilio
-import json
-import subprocess
-import requests
-from urllib.request import urlopen
-from bs4 import BeautifulSoup 
-import re
 import webbrowser
-import pocketsphinx
-from datetime import *
+from datetime import datetime
 
-greetings = ['Welcome back.', 'How are you today?', 
-             'I\'ve missed you.']
+global responses
 responses = ['Yes sir.', 'As you wish.', 'Of course sir.',
              'Just one moment.' 'Right away sir.',
              'As you command.', 'Please wait.']
@@ -45,6 +35,7 @@ responses = ['Yes sir.', 'As you wish.', 'Of course sir.',
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 185)
 global r
 r = sr.Recognizer()
 global mic
@@ -54,32 +45,47 @@ mic = sr.Microphone()
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-     
+    
+
+def greet():
+    greetings = ['Welcome back.', 'How are you today?', 
+             'I\'ve missed you.']
+    if datetime.now().hour >= 6 and datetime.now().hour < 9:
+        speak('Good morning sir.')
+        speak(random.choice(greetings))
+    elif datetime.now().hour >= 9 and datetime.now().hour < 14:
+        speak('Good day sir.')
+        speak(random.choice(greetings))
+    elif datetime.now().hour >= 14 and datetime.now().hour < 18:
+        speak('Good afternoon sir.')
+        speak(random.choice(greetings))
+    elif datetime.now().hour >= 18 and datetime.now().hour < 21:
+        speak('Good evening sir.')
+        speak(random.choice(greetings))
+             
+
 """ Make a recognizer function to start when 
 you say certain words """    
-keywords = [("aku", 1), ('hey aku', 1), ('yo aku',1),]
-
-""" Make a dictionary of different websites with
-their names as keys and  urls as the values 
-"""
-Websites = {'youtube' : 'https://www.youtube.com/',
-    'google' : 'https://www.google.com/',
-    'reddit' : 'https://www.reddit.com/',
-    'td ameritrade' : 'https://www.tdameritrade.com/home.html',
-    'gmail' : 'https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
-    'ncbi' : 'https://www.ncbi.nlm.nih.gov/',
-    'indeed' : 'https://www.indeed.com/',
-    'github' : 'https://github.com/'}
+keywords = [("Jarvis"), ('hey Jarvis') ('yo Jarvis')]
 
 
-def callback(recognizer, audio):
-    try:
-        speech_as_text = recognizer.recognize_sphinx(audio, keyword_entries=keywords) #Uses Sphinx to recognise speech
-        print(speech_as_text) #prints what was said on the screen
-        if "jarvis" in speech_as_text or "hey jarvis": #starter names
-            speak("Yes sir?") #Calls 'Speak' and acknowledges user
-    except sr.UnknownValueError: #if there is nothing understood
-        print("Oops! Didn't catch that") #prints to screen error message
+def rise():
+    calls = ['Yes sir?', 'You called sir?', 'What can I help you with?', 'What do you need?', 'Listening sir']
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        r.recognize_sphinx(audio)
+        for keyword in keywords:
+            if keyword in r.recognize_sphinx(audio):
+                try:
+                    speak(random.choice(calls))
+                    takeCommand()
+                except sr.UnknownValueError:
+                        print("Sphinx could not understand audio")
+                except sr.RequestError as e:
+                    print("Sphinx error; {0}".formate(e))
 
 def takeCommand():
     with mic as source:
@@ -97,17 +103,9 @@ def takeCommand():
         print("Sphinx error; {0}".formate(e))
     return query
 
-print("Loading your AI personal assistant G-One")
-speak("Loading your AI personal assistant G-One")
-wishMe()  
-
-
-""" Step 2: Web surfing.
-Surf and scrape the web for stuff. I will mostly
-just use wikipedia for information Used to open
-up different websites."""
-
-def get_wiki():
+""" Step 2: Web surfing. """
+#Open wikipedia page on something
+def get_wiki(query):
     speak(random.choice(responses))
     split = query.split()
     the_index = -2
@@ -126,32 +124,36 @@ def get_wiki():
     query = "".join(query)
     webbrowser.open("https://en.wikipedia.org/wiki/" + str(query))
     
-def get_google():
+def get_google(query):
     speak(random.choice(responses))
     quer = query.split()   #takes the query and splits it
     for word in quer:   #loops through words in query
         if quer.index(word)%2 == 1: 
             quer.insert(quer.index(word), "+")   #adds in + sign in every odd index
-    quer = ''.join(quer)
+    quer = ''.join(quer)   #joins all words without any spaces
     webbrowser.open('https://www.google.com/' + quer)
-            
-def play_vid():
-    query = takeCommand().lower
-    query = query.split()
-    query.remove('play')
-    query.remove('by')
-    for word in query:   #loops through words in query
-        if query.index(word)%2 == 1: 
-            query.insert(query.index(word), "+")   #adds in + sign in every odd index
-    "".join(query)
-    webbrowser.open('https://www.youtube.com/results?search_query=' + query)
-
+    
+#Open the YouTube search for a video            
+def play_vid(query):
+    speak(random.choice(responses))    
+    quer = query.split()
+    quer.remove('play')
+    quer.remove('by')
+    for word in quer:   #loops through words in query
+        if quer.index(word)%2 == 1: 
+            quer.insert(query.index(word), "+")   #adds in + sign in every odd index
+    "".join(quer)
+    webbrowser.open('https://www.youtube.com/results?search_query=' + quer)
+    
+#Directs to weather.com - 10-day forecast
 def get_weather():
+    speak(random.choice(responses))
     webbrowser.open("https://weather.com/weather/tenday/l/c277dae335f9538e22909aaa3e887daa822dbde134f4379a25f368167c04bbfe")
 
-    
-def wolf():
-    question = takeCommand()
+#Search WolframAlpha for answers to questions  
+def wolf(query):
+    speak(random.choice(responses))
+    question = query
     app_id = 'AQV4H4-8LWH8GRXWG'
     client = wolframalpha.Client(app_id)
     res = client.query(question)
@@ -159,29 +161,36 @@ def wolf():
     speak(answer)
     print(answer)            
 
-inquiries = ['who', 'what', 'where', 'when',
-            'why', 'how']
-
 if __name__ == '__main__':
     clear = lambda: os.system('cls')
+    greet()
+    inquiries = ['who', 'what', 'where', 'when', 'why', 'how']
+    Websites = {'youtube' : 'https://www.youtube.com/',
+    'google' : 'https://www.google.com/',
+    'reddit' : 'https://www.reddit.com/',
+    'td ameritrade' : 'https://www.tdameritrade.com/home.html',
+    'gmail' : 'https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
+    'ncbi' : 'https://www.ncbi.nlm.nih.gov/',
+    'indeed' : 'https://www.indeed.com/',
+    'github' : 'https://github.com/'}
+    
     while True:
+        rise()
         query = takeCommand().lower()
         for site in Websites:
             if site in query:
                 webbrowser.open(Websites[site])
-        speak("Opening " + str(site))
-  #Search WolframAlpha for answers to questions
+                speak("Opening " + str(site))
+        
         for inquiry in inquiries:
             if inquiry in query:
-                wolf()
-  #Open wikipedia page on something
+                wolf(query)
+                
         if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            get_wiki()
-  #Open the YouTube search for a video  
+            get_wiki(query)
+              
         elif "play" in query and 'by' in query: #Play a video on youtube. Format: 'Play {What you want to play here}
-            play_vid()                     #by {artist or uploader}
-  #Directs to weather.com - 10-day forecast
+            play_vid(query)                     #by {artist or uploader}
         elif 'weather' in query:
             get_weather()
                    
