e3268bd17600c246734ece004d0c3b297008ec06
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-14 19:12:34 -0400

    Update voice recognizer.py

--- My AI/voice recognizer.py
+++ My AI/voice recognizer.py
@@ -1,55 +1,51 @@
-""" Make a function that will recognize my voice. If it detects my voice it will run and if it  doesn't detect my voice it won't run. Voices have frequencies to them,
-so I just need Python  to recognize the frequencies of my voice. I did this same thing in NeuroTab. Just read the  wav file as  numpy array.I can do this with numpy
-or pydub or pyaudio. I would need to make a classification neural network that will know whether it's me or not.
-This is completely optional"""
-
-import numpy as np
-import pydub
-import pyaudio
-import time
-import sounddevice as sd     #plays audio files
-import time         #controls time
-from scipy.io.wavfile import write  #write wav files
-from scipy.io.wavfile import read  #write wav files
-
-
-"""Record audio and save it as files to get numpy data. I need to record probably hundreds of different lines with all pronunciations of all sounds in the alphabet. """
+https://peterroelants.github.io/posts/neural-network-implementation-part02/
 
 """Make a classification neural network. Use cross entropy for loss function"""
-
 a = read("my_voice_data.wav")
 inputs = np.array(a[1], dtype = float)
 weights = np.array(np.random.uniform(np.random.randn(), np.random.randn(), len(inputs)))
-global bias
 bias = 1
-target = 1
-inputs
 
-def sigmoid(x):
-    prediction = 1 / (1 + np.exp(-x))
-    return prediction
+t = target
+y = predicted output
+x = input
+i = which input currently on
 
-def deriv_sigmoid(x):
-    return sigmoid(x) * (1 - sigmoid(x))
+def logistic(z): 
+    return 1. / (1 + np.exp(-z))
 
-""" Ouput layer """
-Z = np.dot(inputs, weights) + bias
-prediction = sigmoid(Z)
+# Define the neural network function y = 1 / (1 + numpy.exp(-x*w))
+def nn(x, w): 
+    return logistic(x.dot(w.T))
 
-""" Binary cross entropy loss """
-def loss(target, prediction):
-    loss = -(target*np.log(prediction) + (1-target)*np.log(1-prediction))
-    return loss
+# Define the neural network prediction function that only returns
+#  1 or 0 depending on the predicted class
+def nn_predict(x,w): 
+    return np.around(nn(x,w))
+    
+# Define the loss function
+def loss(y, t):
+    return - np.mean(
+        np.multiply(t, np.log(y)) + np.multiply((1-t), np.log(1-y)))
 
-#Derivative of BCE
-def deriv_BCE(target, prediction):
-    dBCE = (-target/prediction) + ((1-target)/(1-prediction))
-    return dBCE
+ def gradient(w, x, t):
+    """Gradient function."""
+    return (nn(x, w) - t).T * x
 
+ 
+def delta_w(w_k, x, t, learning_rate):
+    """Update function which returns the update for each 
+    weight {w_a, w_b} in a vector."""
+    return learning_rate * gradient(w_k, x, t)
 
+def CEloss(prediction):
+    for i in range(len(inputs)):
+        sumCE += 1*np.log(prediction) + (1-1)*np.log(1-prediction)    
+        sumCE *= -1
 
+Change in weight:
+    alpha * inputs[i] * (predicted - target)
 
-
-        
-        
+    
+    
     
