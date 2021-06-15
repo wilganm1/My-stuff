cc2ffaf5cbdce842f15de39041583bbb4e541175
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-14 20:32:30 -0400

    Update split, add, join a string.py

--- misc./split, add, join a string.py
+++ misc./split, add, join a string.py
@@ -1,10 +1,9 @@
-""" Let's say you have a string and you want to add something to it, like a word. Here's how """
+""" How to add something between every word in a list """
 
 a_string = "What is the speed of light?"   #String I picked. Use whatever you want.
-
-b_string = a_string.split()      #splits the string into a list of all the words --- ['What','is',...,'light?']
+b_string = a_string.split()      #splits the string into a list of all the words ---> ['What','is','the','speed','of','light?']
 
 for i in b_string:   #loops through words in string
     if b_string.index(i)%2 == 1:   #Gets the index of the word and checks if it's odd or even. %2 == 1 means it's odd.
       b_string.insert(b_string.index(i), "#insert whatever here")   #inerts whatever you want at all the odd indices
-" ".join(b_string)          #This joins all the words together. YOU MUST HAVE A SPACE BETWEEN THE "" MARKS OR THE WORDS WON'T BE SEPARATE
+" ".join(b_string)          #This joins all the words together. YOU MUST HAVE A SPACE BETWEEN THE "" MARKS OR THE WORDS WON'T BE SEPARATE.
