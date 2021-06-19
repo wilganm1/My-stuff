87aa6393ff907110037c0b720b6f2e5fd0e60b55
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-19 19:37:24 -0400

    Update main.py

--- My AI/main.py
+++ My AI/main.py
@@ -7,50 +7,55 @@ import subprocess
 import wolframalpha
 import random
 
-global term
+global password       #Password to get the program going. (Optional)
+password = "shubbery"     #Monty Python reference
+global term      #What you want the AI to address you as.
 term = 'sir'
-global name
+global name     #Name of the AI. Change this to whatever you want.
 name = "jarvis"
-global keywords
-keywords = ["Hey Jarvis", "Yo Jarivs", "Jarvis"]
-global calls
-calls = [f'yes {term}?', 'The fuck do you want now?', 'You called?', 'You rang?', 'shut your whore mouth', 'Yes masta?']
-global responses
-responses = [f'Yes {term}.', 'As you wish.', f'Of course {term}.',
-          'Just one moment.', f'Right away {term}.', 'As you command.', 
-          'One moment please.', 'Please wait.', 'One moment please.']       #Responses so I know it's doing what I ask
-global inquiries
-inquiries = ['who', 'what', 'where', 'when', 'why', 'which','what\'s','whose','when\'s',]   #For wolfram to answer questions
-global Websites
+global calls   #What the AI says when I call it. For the activate() function
+calls = [f'yes {term}?', 'You called?', 'You rang?', 'What can I help you with?']
+
+global responses      #Responses to what I say so I know it's doing what I ask.
+responses = [f'Yes {term}', 'As you wish', f'Of course {term}',
+          'Just one moment', f'Right away {term}', 'As you command', 
+          'One moment please', 'Please wait']       
+global inquiries        #For wolfram to answer questions
+inquiries = ['who', 'what', 'where', 'when', 'why', 'which','what\'s','whose','when\'s',]   
+global Websites    #Preset websites that the computer will open for you.
 Websites = {'youtube' : 'https://www.youtube.com/',
             'google' : 'https://www.google.com/',
             'reddit' : 'https://www.reddit.com/',      #List of websites I've pre-programmed to open when I ask for it.
             'td ameritrade' : 'https://www.tdameritrade.com/home.html',   
-            'gmail' : 'https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin',
             'ncbi' : 'https://www.ncbi.nlm.nih.gov/',
             'indeed' : 'https://www.indeed.com/',
             'github' : 'https://github.com/'}
-global Programs
+global Programs        #Programs on your computer that you can preset. 
 Programs = {"Fret finder": 'C:\\Users\\WilganZMT\\Desktop\\Fret Finder.exe', 
-                 "Audacity": "C:\\Program Files (x86)\\Audacity\\audacity.exe",  
-                 "Krita": "C:\\Program Files\\Krita (x64)\\bin\\krita.exe",
-                 "iTunes": "C:\\Program Files\\iTunes\\iTunes.exe",
-                 'tux guitar': "C:\\Program Files (x86)\\tuxguitar-1.1\\tuxguitar.exe"}
-global instructions
+            "Audacity": "C:\\Program Files (x86)\\Audacity\\audacity.exe",  
+            "Krita": "C:\\Program Files\\Krita (x64)\\bin\\krita.exe",
+            "iTunes": "C:\\Program Files\\iTunes\\iTunes.exe",
+            'tux guitar': "C:\\Program Files (x86)\\tuxguitar-1.1\\tuxguitar.exe"}
+global instructions   #Used to look up a video on how to do something. Used for function
 instructions = ["show me how to", "look up how to", "How do you"]
 
-def speak(text):
+def password():    #Has you enter a password to begin. Uncomment to ativate
+    while 1:
+        if input("Please enter password: ") == password:
+            speak("Welcome back sir")
+        else:
+            speak("Invalid password.")
+            continue
+def speak(text):         #speaks what is typed in
     engine=pyttsx3.init('sapi5')
     voices=engine.getProperty('voices')
-    engine.setProperty('voice',voices[0].id)
+    engine.setProperty('voice',voices[0].id)   #Defaults are 2 voices. It possible to add more. I've done it.
     engine.setProperty('rate', 160)
     engine.setProperty('volume', 1.5)
     engine.say(text)
     engine.runAndWait()
-    
-
-def greet():
-        greetings = ['Welcome back.']
+def greet():       #Says hello to you
+        greetings = ['Welcome back.', 'How are you doing?', "I hope you're well"]
         while True:
             if datetime.now().hour >= 6 and datetime.now().hour < 9:
                 speak(f'Good morning {term}' )
@@ -69,26 +74,28 @@ def greet():
                 speak(random.choice(greetings))
             break      
 def activate():
-    while 1:
+    while 1:       #loop so it stays in this function forever until condition met.
         r=sr.Recognizer()
-        with sr.Microphone() as source:
-            r.adjust_for_ambient_noise(source)
-            print("Awaiting request...")
+        with sr.Microphone() as source:  #sets your default microphone as the source
+            r.adjust_for_ambient_noise(source)   #checks for ambient noise. Only works when it hears you
+            r.pause_threshold = 1      #waits a bit.
+            print("Awaiting request...")   #Prints to the screen so you know it's listening
             audio=r.listen(source)
             try:
-                spoken = r.recognize_google(audio,language='en-in')
-                if f"{name}" in spoken.lower():
-                    break
+                spoken = r.recognize_google(audio,language='en-in')   #What it hears and transcribes. 
+                if f"{name}" in spoken.lower():  #Checks if the AI's name is in spoken.
+                    break    #stops the loop,
             except Exception as e:
                 speak("Pardon me, please say that again")
                 continue                   
-def takeCommand():
+def takeCommand():       #Main function of the AI. listens to you speak and will pass it along.
     while True:
         r=sr.Recognizer()
         with sr.Microphone() as source:
             r.adjust_for_ambient_noise(source)
+            r.pause_threshold = 1
             speak(random.choice(calls))
-            print("Listening...")
+            print("Listening...")    #waiting for a command
             audio=r.listen(source)
             try:
                 query=r.recognize_google(audio,language='en-in')
@@ -96,10 +103,10 @@ def takeCommand():
                 return query
                 break
             except Exception as e:
-                speak("Pardon me, please say that again")
+                speak("Pardon me, please say that again")  #Error if it doesn't understand what you said.
                 continue
-def open_site(query):
-        speak(random.choice(responses))
+def open_site(query):        #opens a website based on what you said. #Format: "Go to/Open up/Open {site}. If not in Websites say .com/.org/.gov
+        speak(random.choice(responses))     #random response so you know it's working
         for site in Websites:    #checks if a pre-determined site is in query
             if site in query:    
                 speak('Opening ' + site) 
@@ -108,9 +115,9 @@ def open_site(query):
         for x in extensions:
             if x in query:
                 extension = x    #Save this for later use in webbrowser
-                query = query.replace(x, "")  #gets rid of the extension.
-        quer = query.split()           #Splits the query into separate words
-        if 'open' == quer[0]:  #Following code just removes words if they're in the query
+                query = query.replace(x, "")    #gets rid of the extension.
+        quer = query.split()             #Splits the query into separate words
+        if 'open' == quer[0]:     #Following code just removes words if they're in the query
             quer.remove('open')
         elif 'open' == xyz[0] or 'go' == xyz[0] and 'to' == xyz[1]:
             quer.remove('go')
@@ -120,35 +127,35 @@ def open_site(query):
         print('Opening ' + quer)
         webbrowser.open("https://www." + quer + str(extension))    #opens the website in browser   
         return
-def instruction_vid1(query):
+def instruction_vid1(query):   #Opens a youtube video on how to do something: "(Show me/look up) How to use a chainsaw"
     speak(random.choice(responses))
     if instructions[0] in query or instructions[1] in query:
-        quer = query.split()
+        quer = query.split()     
+        quer.remove(quer[0])  #removes "look up/show me"
         quer.remove(quer[0])
-        quer.remove(quer[0])
-        for word in quer:   #loops through words in query
-            if quer.index(word)%2 == 1: 
+        for word in quer:    #loops through words in query
+            if quer.index(word)%2 == 1:      #checks if the index is odd.
                 quer.insert(quer.index(word), "+")   #adds in + sign in every odd index
         quer="".join(quer)
-        webbrowser.open('https://www.youtube.com/results?search_query=' + quer)      
-def instruction_vid2(query):
+        webbrowser.open('https://www.youtube.com/results?search_query=' + quer)   #Opens youtube search for query  
+def instruction_vid2(query):      #Same thing as previous functio.  Format: "How do you {whatever}
     speak(random.choice(responses))
-    quer = query.split()
+    quer = query.split()           ##['How', 'do', 'you', ~~~~~~~~~~]
     quer.remove("do")
     quer.remove("you")
-    quer.insert(quer.index('how')+1, 'to')
+    quer.insert(quer.index('how')+1, 'to')   #adds 'to' to the list after 'how'.
     for word in quer:   #loops through words in query
         if quer.index(word)%2 == 1: 
-            quer.insert(quer.index(word), "+")   #adds in + sign in every odd index
-    quer ="".join(quer)
+            quer.insert(quer.index(word), "+")   
+    quer ="".join(quer)  
     webbrowser.open('https://www.youtube.com/results?search_query=' + quer)
     return
-def open_program(program):
+def open_program(program):         #Opens program on your computer that's in the Programs dictionary
     subprocess.call(Programs[program])
     return
-def wolf(query):
+def wolf(query):    #Checks wolframalpha for a answers to a question. Wolframalpha is a search engine. 
         speak("Checking")
-        app_id = 'AQV4H4-8LWH8GRXWG'             
+        app_id = ''      #You need to create an account and get your own app_id.       
         client = wolframalpha.Client(app_id)
         res = client.query(query)
         answer = next(res.results).text
@@ -164,78 +171,78 @@ def wolf(query):
             print(answer)  
             speak("There are " + answer + " " + quer)
         return
-def get_weather():
+def get_weather():      #Opens weather.com for your location. Add link of your location.
         speak(random.choice(responses))
-        webbrowser.open("https://weather.com/weather/tenday/l/c277dae335f9538e22909aaa3e887daa822dbde134f4379a25f368167c04bbfe")
+        webbrowser.open("https://weather.com")
         return
-def get_wiki(query):     
+def get_wiki(query):     #Opens wikipedia page for something. Format: "(Open/Search/~~) wikipedia {for/on/about} (whatever)"
         speak(random.choice(responses))    
         split = query.split()                                              
-        the_index = -1
-        while True:
+        the_index = -1           #Starts at the last index .
+        while 1:     
             if split[the_index] != 'for' or split[the_index] != 'on' or split[the_index] != 'about':
-                the_index -= 1
+                the_index -= 1   #Checks the split for certain words that need to bethere. If they are not the index goes back one.
                 continue
             elif split[the_index] == 'for' or split[the_index] == 'on' or split[the_index] == 'about':
-                query = split[the_index+1:]
-                break
-        for i in range(len(query)):
-            query[i] = query[i].capitalize()
-        for word in query:   #loops through words in query
-            if query.index(word)%2 == 1: 
-                query.insert(query.index(word), "_") 
-        query = "".join(query)
-        webbrowser.open("https://en.wikipedia.org/wiki/" + query)
+                quer = split[the_index+1:]  #This returns only what's after for/on/about. Exmaple: for the big bang
+                break       
+        for i in range(len(quer)):
+            quer[i] = quer[i].capitalize()    #The Big Bang
+        for word in quer:      #loops through words in query
+            if quer.index(word)%2 == 1:  #chekcs for odd indices
+                quer.insert(quer.index(word), "_")   #inserts _ for the url
+        quer = "".join(quer)     #The_Big_Bang
+        webbrowser.open("https://en.wikipedia.org/wiki/" + quer)   #opens the wikipedia page for query
         return
-def play_vid(query):
+def play_vid(query):       #Searches youtube for a video
         speak(random.choice(responses))    
-        quer = query.split()
+        quer = query.split()   # "Play {video} by {uploader/artist}"
         quer.remove('play')
         quer.remove('by')
-        for word in quer:   #loops through words quer
+        for word in quer:   
             if quer.index(word)%2 == 1: 
-                quer.insert(quer.index(word), "+")   #adds in + sign in every odd index
+                quer.insert(quer.index(word), "+")
         quer = "".join(quer)
         webbrowser.open('https://www.youtube.com/results?search_query=' + quer)
         return
 if __name__=='__main__':   
-    greet()
-    try:
-        while 1:
-            activate()
-            query = takeCommand().lower()
-            xyz = query.split()
-            for inquiry in inquiries:
-                if inquiry == xyz[0]:
-                    wolf(query)
-                    continue
-            for program in Programs:
-                if 'open' == xyz[0] and program in xyz:
-                    open_program(program)
-                    continue
-            if 'wikipedia' in query:
-                get_wiki(query)
+    #password()       
+    greet()            
+    while 1:           #Starts a loop that will keep the script running unless it's told to end
+        activate()       
+        query = takeCommand().lower()     
+        xyz = query.split()        #splits the query to check for certain words. Used for later.
+        for inquiry in inquiries:     
+            if inquiry == xyz[0]:     #Checks if a word in inquiries is the first word of the query
+                wolf(query)       
+                continue     #This loops back to the start of the loop. 
+        for program in Programs:
+            if 'open' == xyz[0] and program in xyz:
+                open_program(program)
                 continue
-            elif 'the weather' in query:   #As long as "the weather" is in query it doesn't matter.
-                get_weather()
-                continue
-            elif '.com' in query or '.org' in query or '.gov' in query and 'open' == xyz[0] or 'go' == xyz[0] and 'to' == xyz[1]:
-                open_site(query)     #Format: "Go to/Open up/Open {site}. If not in Websites say .com/.org/.gov
-                continue
-            elif'how' == xyz[0] and 'do' == xyz[1] and 'you' == xyz[2]:
-                instruction_vid2(query)
-                continue
-            elif 'how' == xyz[2] and 'to' == xyz[3]:
-                instruction_vid1(query)             #Format: "Look up how to/Show me how to/How do you
-                continue
-            elif 'how many' in query or 'how much' in query:
-                wolf(query)
-                continue
-            elif "play" == xyz[0] and 'by' in query:  #Format: "Play {video} by {uploader/artist}
-                play_vid(query)
-                continue       
-            elif "end program" in query:
-                speak("Alright the. Goodbye.")
-                break
-    except RuntimeError:
-        os._exit(0)
+        if 'wikipedia' in query: 
+            get_wiki(query)
+            continue
+        elif 'the weather' in query:   
+            get_weather()
+            continue
+        elif '.com' in query or '.org' in query or '.gov' in query and 'open' == xyz[0] or 'go' == xyz[0] and 'to' == xyz[1]:
+            open_site(query)     
+            continue          
+        elif 'how' == xyz[2] and 'to' == xyz[3]:  
+            instruction_vid1(query)    
+            continue
+        elif'how' == xyz[0] and 'do' == xyz[1] and 'you' == xyz[2]:
+            instruction_vid2(query)     
+            continue
+        elif 'how many' in query or 'how much' in query:
+            wolf(query)
+            continue
+        elif "play" == xyz[0] and 'by' in query:  
+            play_vid(query)  
+            continue       
+        elif "end script" in query or "end program" in query or 'kill yourself' in query:
+            speak("Alright the. Goodbye.")  #Causes the program to break and end
+            break
+        elif RuntimeError:
+            os._exit(0)
