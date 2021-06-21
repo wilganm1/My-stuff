429cfd41c8f1d30d6895f0cfd27a9cfc08b24b11
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-20 22:00:41 -0400

    Update main.py
    Fixed the wallpaper function so it works now.

--- My AI/main.py
+++ My AI/main.py
@@ -7,6 +7,8 @@ import subprocess
 import wolframalpha
 import random
 import ctypes
+from os import walk        #!!!! Have the names of your images something that is easy to read. dog/space station/grass field/Earth, etc.
+
 
 global term      #!!! What you want the AI to address you as.
 term = 'sir'
@@ -32,7 +34,7 @@ global instructions   #Used to look up a video on how to do something. Used for 
 instructions = ["show me how to", "look up how to", "How do you"]
 
 def password():    #Has you enter a password to begin. Uncomment below to activate
-    password = "shubbery"     #Monty Python reference
+    password = "shrubbery"     #Monty Python reference
     while 1:
         if input("Please enter password: ") == password:
             speak("Welcome back sir")
@@ -42,7 +44,7 @@ def password():    #Has you enter a password to begin. Uncomment below to activa
 def speak(text):         #speaks what is typed in
     engine=pyttsx3.init('sapi5')
     voices=engine.getProperty('voices')
-    engine.setProperty('voice',voices[0].id)   #Defaults are 2 voices. It possible to add more. I've done it.
+    engine.setProperty('voice',voices[3].id)   #Defaults are 2 voices. It possible to add more. I've done it.
     engine.setProperty('rate', 160)    #How fast the voice speaks.
     engine.setProperty('volume', 1.5)  #how loud the voice is.
     engine.say(text)
@@ -125,7 +127,7 @@ def open_site(query):        #opens a website based on what you said. #Format: "
 def instruction_vid1(query):   #Opens a youtube video on how to do something: "(Show me/look up) How to use a chainsaw"
     speak(random.choice(responses))
     if instructions[0] in query or instructions[1] in query:
-        quer = query.split()     
+        quer = query.split()     #['Look','up','how','to] / ['show','me','how','to']
         quer.remove(quer[0])  #removes "look up/show me"
         quer.remove(quer[0])
         for word in quer:    #loops through words in query
@@ -133,9 +135,9 @@ def instruction_vid1(query):   #Opens a youtube video on how to do something: "(
                 quer.insert(quer.index(word), "+")   #adds in + sign in every odd index
         quer="".join(quer)
         webbrowser.open("".join(['https://www.youtube.com/results?search_query=', quer]))   #Opens youtube search for query  
-def instruction_vid2(query):      #Same thing as previous functio.  Format: "How do you {whatever}
+def instruction_vid2(query):      #Same thing as previous function.  Format: "How do you {whatever}
     speak(random.choice(responses))
-    quer = query.split()           ##['How', 'do', 'you', ~~~~~~~~~~]
+    quer = query.split()           #['How', 'do', 'you', ~~~~~~~~~~]
     quer.remove("do")
     quer.remove("you")
     quer.insert(quer.index('how')+1, 'to')   #adds 'to' to the list after 'how'.
@@ -150,7 +152,7 @@ def open_program(program):         #Opens program on your computer that's in the
     return
 def wolf(query):    #Checks wolframalpha for a answers to a question. Wolframalpha is a search engine. 
         speak("Checking")
-        app_id = ''      #You need to create an account and get your own app_id.       
+        app_id = ''      #!!! You need to create an account and get your own app_id. type in wolfram alpha in google and make an account there.       
         client = wolframalpha.Client(app_id)
         res = client.query(query)
         answer = next(res.results).text
@@ -202,30 +204,27 @@ def play_vid(query):       #Searches youtube for a video
         webbrowser.open("".join(['https://www.youtube.com/results?search_query=',quer]))
         return
 def change_wallpaper():  #Changes wallpaper. You need a folder that has pics you want to choose from.  
-    from os import walk        #!!!! Have the names of your images something that is easy to read. dog/space station/grass field/Earth, etc.
-    mypath = "C:\\Users\\~~~~~~~\\Pictures\\Wallpaper pics"  #!!! Insert the folder that has the images you want to choose from
-    filenames = next(walk(mypath), (None, None, []))[2]  # [] if no file
-    image_type = ['.jpg','.png']    #list of image types. Used for later.
-    for x in filenames:    
-        for i in image_type:
-            if i in x:       #checks if the image has a known extension
-                img = i
-                x.replace(i, "")  #Gets rid of .jpg/.png/whatever.
-                print(x)      #prints out names of files
-    speak("Please speak desired image")
     while 1:       #loop so it stays in this function forever until condition met.
         r=sr.Recognizer()
+        mypath = "C:\\Users\\WilganZMT\\Pictures\\Wallpaper pics"  #!!! Insert the folder that has the images you want to choose from
+        from os import listdir
+        from os.path import isfile, join
+        onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        for x in onlyfiles:
+            x = x.replace('.jpg', "")
+            print(x)
         with sr.Microphone() as source:  
             print("Choose wallpaper...")   #Prints to the screen so you know it's listening
+            speak("Please speak desired image")
             audio=r.listen(source)
             try:
                 wallpaper = r.recognize_google(audio,language='en-in')   
-                if wallpaper in filenames:  #Checks if the wallpaper is in the folder
-                    ctypes.windll.user32.SystemParametersInfoW(20,0, "".join([mypath,"\\", wallpaper, img]), 0)     #This is used for windows.                      
-                    break    #stops the loop
-                else:
-                    speak("File not found")
-                    break
+                print(f"user said: {wallpaper}\n")
+                for x in onlyfiles:    
+                    x = x.replace('.jpg', "")  #Gets rid of .jpg/.png/whatever.
+                    if x == wallpaper:
+                        ctypes.windll.user32.SystemParametersInfoW(20,0, "".join([mypath,"\\", str(x), ".jpg"]), 0)     #This is used for windows.                      
+                        break    #stops the loop
             except Exception as e:
                 speak("Pardon me, please say that again")
                 continue  
@@ -256,7 +255,7 @@ if __name__=='__main__':
         elif '.com' in query or '.org' in query or '.gov' in query and 'open' == xyz[0] or 'go' == xyz[0] and 'to' == xyz[1]:
             open_site(query)     
             continue          
-        elif 'how' == xyz[2] and 'to' == xyz[3]:  
+        elif 'how' in query and xyz.index('to') == xyz.index('how')+1:  
             instruction_vid1(query)    
             continue
         elif'how' == xyz[0] and 'do' == xyz[1] and 'you' == xyz[2]:
@@ -272,7 +271,11 @@ if __name__=='__main__':
             change_wallpaper()
             continue
         elif "end script" in query or "end program" in query or 'and script' in query or 'and program' in query or 'kill yourself' in query:
-            speak("Alright the. Goodbye.")  #Causes the program to break and end
+            speak("Alright then. Goodbye.")  #Causes the program to end
             break
         elif RuntimeError:
             os._exit(0)
+        else:
+            break
+    if RuntimeError:
+        os._exit(0)
