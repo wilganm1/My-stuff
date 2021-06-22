8fb00a4c3389cd2497741d4fe862916d7b98693d
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-06-22 17:22:13 -0400

    Update better fretboard.py
    Made it longer.

--- Fret finder/better fretboard.py
+++ Fret finder/better fretboard.py
@@ -1,49 +1,62 @@
 from tkinter import *
+
 root = Tk()
+
+frame4 = Frame(master = root)
 
 """ Make a fretboard that spans the width of the 
 root frame. Then put in strings and frets and nut. """
-frame4 = Frame(master = root)
-canvas = Canvas(frame4, width = 510, height = 95)
-neck_length = 480
+canvas = Canvas(frame4, width = 600,
+                height = 95)
+
+neck_length = 600
 fret_length = neck_length - 15
+
 """ Neck """
 canvas.create_rectangle(0,0, neck_length, 95,
                 outline="#5c3626", fill="#5c3626")
 """ Strings """
 for i in range(0,6):
     canvas.create_rectangle(0, (i*15)+10, neck_length, (i*15)+10)
+
 """ Frets """
 for fret in range(1,25):
      canvas.create_rectangle(fret * (fret_length/24), 10, (fret * (fret_length/24)), 85,
             outline = "#b8b0ad", fill = "#b8b0ad")
+
 """ Nut """
 canvas.create_rectangle(0,10,5,85,
-        outline = "black", fill="black")     
+        outline = "black", fill="black")
+     
 """ Fret circles """ 
 for i in [3,5,7,9,15,17,19,21]: #Frets 3,5,7,9
-    canvas.create_oval(((i-1)*(fret_length/24)) + 6, 42.5,
-                       ((i-1)*(fret_length/24)) + 16, 44.5 + height_radius,
+    canvas.create_oval(((i-1)*(fret_length/24)) + 7.5, 42.5,
+                       ((i-1)*(fret_length/24)) + 17.5, 52.5,
                        outline = "white", fill = "white")
         #Fret 12
-canvas.create_oval(218.5, 27.5, 228.5, 37.5,
+canvas.create_oval(11 * (fret_length/24) + 7.5, 27.5, 11 * (fret_length/24) + 17.5, 37.5,
             outline = "#f1f0f0", fill="#f1f0f0")  
-canvas.create_oval(218.5, 57.5, 228.5, 67.5,
+canvas.create_oval(11 * (fret_length/24) + 7.5, 57.5, 11 * (fret_length/24) + 17.5, 67.5,
             outline = "#f1f0f0", fill="#f1f0f0")
+
         #Fret 24
-canvas.create_oval(452, 27.5, 462, 37.5,
+canvas.create_oval(23 * (fret_length/24) + 7.5, 27.5, 23 * (fret_length/24) + 17.5, 37.5,
             outline = "#f1f0f0", fill="#f1f0f0")  
-canvas.create_oval(452, 57.5, 462, 67.5,
+canvas.create_oval(23 * (fret_length/24) + 7.5, 57.5, 23 * (fret_length/24) + 17.5, 67.5,
             outline = "#f1f0f0", fill="#f1f0f0")
-   
-""" Problem 3:
+
+    
+""" Problem:
     The actual circles don't update. Instead of the circles changing, 
     the program just adds in another fretboard. So everytime you click the
     "Get" button it adds a new fretboard. Need to find a way to fix that.
 
-    I added in the clear() function which gets rid of the fret board
+    I added in a canvas.delete('all') function which got rid of the fret board
     but does update the dots. 
-    """  
+    
+    I need to simutaneously delete the dots then update it.
+    """
+  
 for io in range(len(results)):
     for ng in results[io]:
         if ng == 0:
@@ -87,6 +100,7 @@ for io in range(len(results)):
                 ((io * 15) +10) + 5,
                 outline = 'orange', fill='orange')
 canvas.pack()
-frame4.pack()
-root.mainloop()                  
 
+frame4.pack()
+
+root.mainloop()                  
