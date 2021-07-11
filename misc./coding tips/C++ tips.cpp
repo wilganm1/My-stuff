5ae2d0847b70354d2f9d18c50cf3404834d03a51
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-07-11 12:13:49 -0400

    Update C++ tips.cpp

--- misc./coding tips/C++ tips.cpp
+++ misc./coding tips/C++ tips.cpp
@@ -1,108 +1,117 @@
 C++ tips:
 You can import librarys like in Python.
 #include <iostream>
-using namespace std
-#include <string>
 #include <cmath>     //include cmath library
+using std::cout    //Don't use the entire standard namespace.
+using std::cin     //Use this as a shortcut.
 
--Variables
+-Variable Types
 int          -stores integers.
 double,float -stores decimal numbers.
 char         -stores single characters only. Uses ''
-string       -stores text lines. Uses "". Need to #include <string> library
+string       -stores text lines. Uses "".
 bool         -True or False vales. 1= true, 0 = false
 
-
 -Operators
-=          -assigns values to variables
-+          -adds numbers. Concatenates strings.
--          -subtracts things.
-*          -multiplies values
-/          -divides values
-%          -modulus. Returnes division remainder
-++         -increases values by 1
---         -decreases values by 1
-&&         -and operator. compares things. Like and in Python
-||         -or operator. like in Python.
-!          -logical not
-::         -scope resolution operator
->          -greater than. Returns true or false
-<          -less than.
->=         -greater than or equal to.
-<=         -less than or equal to.
-==         -is equal to
-!=         -not equal to
-+=         -adds value to a variables
--=         -subtracts from a variables
-*=         -multiplies a variable
-&=         -reference equals
-|=
-^=
->>=
-<<=
+=      -assigns values to variables
++      -adds numbers. Concatenates strings.
+-      -subtracts things.
+*      -multiplies values
+/      -divides values
+%      -modulus. Returnes division remainder
+++     -increases values by 1
+--     -decreases values by 1
+&&     -and operator. compares things. Like and in Python
+||     -or operator. like in Python.
+!      -logical not
+::     -scope resolution operator
+>      -greater than. Returns true or false
+<      -less than.
+>=     -greater than or equal to.
+<=     -less than or equal to.
+==     -is equal to
+!=     -not equal to
++=     -adds value to a variables
+-=     -subtracts from a variables
+*=     -multiplies a variable
+&=     -reference equals
 
 All comparison operators need to be in (). (~ > ~)
 
 -Syntax
-cout <<      -outputs values. works like print() in Python.
-return 0;    -Ends the function
-<<           -"See out". Output
->>           -"see in". Input
-;            -ends statements in C++
-\n           -inserts a new line.
-\n\n, endl   -creates a blank line
-//           -single-line comments. Ignored by compiler. Like # in Python.
-/* ~~ */     -multi-line comments. Like """ in Python.
-std::        -can bypass the namespace.
+ cout <<      -outputs values. works like print() in Python.
+ <<           -"See out". Output
+ >>           -"see in". Input
+ ;            -ends statements in C++
+ \n           -inserts a new line.
+ \n\n, endl   -creates a blank line
+ //           -single-line comments. Ignored by compiler. Like # in Python.
+ /* ~~ */     -multi-line comments. Like """ in Python.
+ std::        -can bypass the standard namespace library.
 
 -Variable creation
   type identifier = value;
     int myNum = 15;
     cout << myNum;
-You can combine variables in one cout sentence
+ You can combine variables in one cout sentence
         int myAge = 35;
         cout << "I am " << myAge << " years old.";
-Add variables together:
+ Add variables together:
     int x = 5;
     int x = 6;
     int sum = x + y;
     cout << sum;
 
-Declare multiple variables of the dame type.
+ Declare multiple variables of the dame type.
     int x = 4, y = 5, z = 20;
     cout << x+y+z;
 
-Constants, unchanging variables
+ Constants, unchanging variables
     const int myNum = 15;  -use const to make it a constant.
 
-Use e to represent powers of 10 for numbers
+ Use e to represent powers of 10 for numbers
     float f1 = 35e3;
 
-Reference operator. &
-you can create another variable with the same value as another variable
-    string food = "Pizza";
-    string &meal = food;
+ Reference operator. &
+  you can create another variable with the same value as another variable
+     string food = "Pizza";
+     string &meal = food;
 
-    cout << food << "\n";  // Outputs Pizza
-    cout << meal << "\n";  // Outputs Pizza
+     cout << food << "\n";  // Outputs Pizza
+     cout << meal << "\n";  // Outputs Pizza
 
- Memory addres
+ Memory address &. when you create a variable reference itself to get
+    its memory address
   string food = "Pizza";
-
   cout << &food; // Outputs 0x6dfed4
-*************************************************
- Pointer. stores memory address as its value
+
+ Pointer. stores memory address as its value. Needs a & in its value
   string food = "Pizza";  // A food variable of type string
-  string* ptr = &food;
- Use *ptr to print out the OG value that is being pointed to.
-    *ptr -> 'Pizza'
+  string* ptr = &food;   //NEED TO HAVE & SYMBOL
+ Use *ptr to print out the OG value that is being referenced to. Without
+ the * symbol it just prints out the memory of the referenced variable
+   cout << ptr << endl;  //outputs 0x61fde0
+   cout << *ptr;    //outputs Pizza
+
+
+-Memory allocation. You need to manually deallocate memory in C++.
+  The new operator allocates memory to a variable
+   Syntax: pointerVariable = new dataType
+
+    int* pointVar; // declare an int pointer
+    pointVar = new int; // dynamically allocate memory using new keyword
+    *pointVar = 45; // assign value to allocated memory
+   the output of *pointVar = 45
+
+  delete operator deallocates memory.
+     delete pointerVariable
 
 -User inputs
-cin >>   -Use cin to get user input. Like input() in Python.
- int x;
- cout << "Type a number: ";
- cin >> x;  //Gets user input from keyboard
- cout << "Your number is: " << x;
+ cin >>   -Use cin to get user input. Like input() in Python.
+  int x;
+  cout << "Type a number: ";
+  cin >> x;  //Gets user input from keyboard
+  cout << "Your number is: " << x;
 If you want to save a line of text to cin use getline(cin, x)
     string fullName;
     cout << "Type your full name: ";
@@ -111,98 +120,105 @@ If you want to save a line of text to cin use getline(cin, x)
 
 Concatenate strings.
 Use + to combine different strings. Add a space at end of first string " "
-or you can use .appen() as the same thing.
+or you can use .append() as the same thing.
 
 -Indexing strings []
-Can access the element at a certain posistio. 0-indexed like Python.
-   string myString = "Hello";
-   cout << myString[1];
-   //Outputs e
+ Can access the element at a certain posistiom. 0-indexed like Python.
+    string myString = "Hello";
+    cout << myString[1];
+    //Outputs e
 -Methods
-Methods are built-in functions like python.
-x.append(y)  -concatenates y to end of x
-x.length()   -gets length of x
-max(x,y,..)  -returns the largest number of x or y
-max(x,y,..)  -returns the smallest number of x or y
+    Methods are built-in functions like python.
+    x.append(y)  -concatenates y to end of x
+    x.length()   -gets length of x
+    max(x,y,..)  -returns the largest number of x or y
+    max(x,y,..)  -returns the smallest number of x or y
 
 -Conditional statements
-if       -block of code is run if condition is true
-else     -runs if same condition is false
-else if  -runs if first condition is false
-switch   -specify many alternative blocks of code to be executed
+    if       -block of code is run if condition is true
+    else     -runs if same condition is false
+    else if  -runs if first condition is false
+    switch   -specify many alternative blocks of code to be executed
 
-if (condition1) {//block of code run if true
-} else if (condition2){//block of code if condition1 is false
-} else { //block of code run if all are false
-}
-Ternary ? operator as shortcut for if else
-    (condition) ? expressionTrue : expressionFalse
-  string result = (x < 18) ? "x is illegal." : "x is legal.";
-Switch syntax
-switch(parameter) {    //loops through cases
-  case x:
-    // code block if x == parameter
-    break;   //stops the loop
-  case y:
-    // code block if y == parameter
-    break;
-  default:
-    // code block if nothing == parameter
-}
-switch will evaluate an expression and will run the case that mathces it.
+    if (condition1) {//block of code run if true
+    } else if (condition2){//block of code if condition1 is false
+    } else { //block of code run if all are false
+    }
+    Ternary ? operator as shortcut for if else
+        (condition) ? expressionTrue : expressionFalse
+      string result = (x < 18) ? "x is illegal." : "x is legal.";
+
+    -Switch syntax. Basically a giant if, else if layout.
+    switch(expression) {    //loops through cases
+      case x:
+        // code block if x == expression
+        break;   //stops the loop
+      case y:
+        // code block if y == expression
+        break;
+      default:
+        // code block if nothing == expression
+    }
+    switch will evaluate an expression and will run the case that mathces it.
 
 -Loops, for and while
-While loops will run forever unless a condition is met.
-int i = 0;
-while (i < 5) {
-  cout << i << "\n";
-  i++;   //increases i every time. Then restarts the loop
-}
+ While loops will run forever unless a condition is met.
+   int i = 0;
+   while (i < 5) {
+     cout << i << "\n";
+     i++;}   //increases i every time. Then restarts the loop
 
-Do/While loop
-will run code block once, checks if condition is true, and will continue
-to loop if the condition is true.
-do {
-  // code block to be executed
-}
-while (condition);  //runs if do == true
+   Multiple conditions with OR. (this OR that)
+    C++ is different than python that if you want to say (this OR that) you need
+    to use the && operator because one is always set to true.
+     (condition 1 && condition 2)
+      Example: while x < 5 OR y < 3
+  -->   while (x < 5 && y < 3)
+
+
+ Do/While loop
+     will run code block once, checks if condition is true, and will continue
+     to loop if the condition is true.
+     do {
+       // code block to be executed
+     }
+     while (condition);  //runs if do == true
 
 For loop. More control over loop. (starting; while condition; change)
-for (int i = 0; i < 5; i++) {
-  cout << i << "\n";
-}
+ for (int i = 0; i < 5; i++) {
+   cout << i << "\n";}
 
 You can combine for, if, and break to stop a loop at a condition
-for (int i = 0; i < 10; i++) {
-  if (i == 4) {
-    break;     //stops the loop entirely
-  }
-  cout << i << "\n";
-}
-continue. skips the iteraion if a condition is met. goes to the next iteration
-for (int i = 0; i < 10; i++) {
-  if (i == 4) {
-    continue;
-  }
-  cout << i << "\n";
-}
+ for (int i = 0; i < 10; i++) {
+   if (i == 4) {
+     break;     //stops the loop entirely
+   }
+   cout << i << "\n";}
+
+  continue. skips the iteraion if a condition is met. goes to the next iteration
+ for (int i = 0; i < 10; i++) {
+   if (i == 4) {
+     continue;
+   }
+   cout << i << "\n";}
 
 -Arrays/lists. Uses {}
-create arrays using [x]. x = how long it is. Need to be the same elem type.
-int myNum[3] = {10,20,30}
-change the value of an elem in a certain index the same way in python.
-cars[0] = "New value"
+ create arrays using dataType arrayName[x]. x = how long it is. Need to be
+ the same elem type.
+ int myNum[3] = {10,20,30}
+ change the value of an elem in a certain index the same way in python.
+ cars[0] = "New value"
 
-you can loop through an arryay.
-string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
-for(int i = 0; i < 4; i++) {
-  cout << i << ": " << cars[i] << "\n";}
+ you can loop through an arryay.
+ string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+ for(int i = 0; i < 4; i++) {
+   cout << i << ": " << cars[i] << "\n";}
 
-Omit elements on declaration
-string cars[5];
-cars[0] = "Volvo";
-cars[1] = "BMW";
-...
+ Omit elements on declaration
+  string cars[5];
+  cars[0] = "Volvo";
+  cars[1] = "BMW";
+  ...
 
 -Functions
 functions are blocks of code that do specific things. Only when called.
@@ -218,7 +234,7 @@ functions are blocks of code that do specific things. Only when called.
   be defined after the main function.
 
   Functions can have parameters that are variables within the function.
-  They can be anythin. Just like python
+  They can be anything. Just like python
 
      void myFunction(string fname) {
       cout << fname << " Refsnes\n";}
@@ -226,6 +242,8 @@ functions are blocks of code that do specific things. Only when called.
     int main() {
       myFunction("Liam"); //function will output "Liam"
       return 0;}
+
+  **********YOU CANNOT DO NESTED FUNCTION IN C++************
 
    -Default variable
    You can add a default variable using =. If the function has no argument
@@ -275,9 +293,6 @@ functions are blocks of code that do specific things. Only when called.
         int year;    };
     int main() {
       // Create an object of Car
-
-      //ClassName objName\\
-
       Car carObj1;
       carObj1.brand = "BMW";
       carObj1.model = "X5";
@@ -334,6 +349,18 @@ functions are blocks of code that do specific things. Only when called.
       brand = x;
       model = y;
       year = z;}
+
+  -Struct. group of data elements together under one name.
+     Like a class but simplified and public as default.
+      struct nameOfStruct {
+        int x;
+        double y;}
+    You can then call objects with attributes of the structure.
+      nameOfStruct a;
+        a.x = 20
+
+
+
 
 -Access specifiers.
 How member of a class can be accessed.
@@ -415,15 +442,5 @@ Use fstream library to work with files
       cout << myText;}
      MyReadFile.close();   // Close the file
 
--Exceptions
-These are errors. Uses three keywords:
-    try    -define a block of code to be tested
-    throw  -creates exception when problem is detected. goes to catch
-    catch  -lets you define a new block of code if error is in try block.
-  try and catch come in pairs. Like try and except in Python.
-     try {    
-         // Block of code to try
-       throw exception;}  // Throw an exception when a problem arise
-     catch () {
-     // Block of code to handle errors
-     } 
+
+-RAII
