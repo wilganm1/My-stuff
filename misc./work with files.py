d4fd78f7e2e52e36ece1749a56d50cc4a81bd7f3
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-07-20 22:46:57 -0400

    Create work with files.py

---
+++ misc./work with files.py
@@ -0,0 +1,42 @@
+""" Working with files """
+import os
+#Know where the file you want to work with is
+
+os.getcwd()
+os.chdir('\\insert path here')
+
+"""Check if file exists """
+if os.path.exists("nameoffile.txt"):
+    print("file exists")
+else:
+    print("The file does not exist")
+
+
+""" Create new file """
+file = open("nameOfFile.txt", "x") #x creates file
+
+""" Overwrite/create file """
+file = open("fileName.txt", "w")
+
+""" Open existing file """ #default
+file = open("file_name.extension", 'r')
+
+""" Append to file """
+file = open("filename.txt", "a")
+
+""" Read the file """
+print(file.read())
+print(file.read(n)) #print n characters
+
+print(file.readline()) #prints one line
+#keep printing readline to read each line
+
+#loop through the file line by line
+for x in file:
+    print(x)
+    
+#close file
+file.close()
+
+#delete file
+os.remove("filename.txt")
