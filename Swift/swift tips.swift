89b0f0742a92f9e12a77990b2a5a21ce1aadbaea
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-09-08 16:36:33 -0400

    Create swift tips.swift

---
+++ Swift/swift tips.swift
@@ -0,0 +1,56 @@
+Swift is a language used for Apple producs using MacOS software. Swift is a type-safe, meaning it prevents you from 
+entering in wrong data values into variables.
+
+Data types
+ Int        -integer. Whole number. (1)
+ Float      -number with decimal points. 32-bit (3.14)
+ Double     -number with decimal points. 64-bit
+ Bool       -true or false
+ String     -plain text. Uses "" ("Hello World")
+ Character  -Single character string.  ("H")
+ Optional   -can either hold a value or not hold a value
+ Tuple      -group multiple values in single compound value
+
+
+Variables & Constants
+ Variables hold data within names and are given a value. 
+ Constants use let and variables use var.
+ You can't change the variable type once declared, only the
+ value if it's the same type
+  Syntax:
+      let variable_name = value_of_varialbe
+      var fasl = eion
+   -Declare multiple variables on the same line with , if same type
+     var x = 1, y = 2, z = 3
+      
+-Type Annotations  
+ Specify what type of data can be housed in the variable.
+    Syntax:
+        var variable_name: data_type
+ 
+-Printing variables or data
+ Use the print() function to print variables
+    Syntax: 
+      print(variable)
+      print("String")      -prints String
+      print(1)             -prints 1
+      
+Comments
+ Comments exclude text from being executed.
+  -Single-line //~~
+      // this is ignored
+  -Multiline /* ~~ */
+      /*   
+        This is ignored
+      */ 
+      
+Semicolons ;
+ Semicolons are used to write separate statements
+ on a single line.
+   Syntax:
+      let cat = "meow"; print(cat)    //prints 'meow'
+
+
+
+
+
