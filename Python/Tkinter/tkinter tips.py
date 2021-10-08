0950428096c8ec5b03e1657312d891cd4fa03778
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-10-08 17:06:22 -0400

    Update tkinter tips.py

--- Python/Tkinter/tkinter tips.py
+++ Python/Tkinter/tkinter tips.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 
 -Creating the window
   root = tk.Tk()      #creates window
@@ -12,13 +13,28 @@ import tkinter as tk
 -Positioning
   .pack()          #Puts the widget in the window in its exact size
   .grid(row = x, column = y, columnspan = z, rowspan = a)
+  root.rowconfigure(n, minsize = x)    #n is which row
+  root.columnconfigure([a,b,c], maxsize = y)  #you can have a list of columns/rows.
 
+-Frames
+  frame1 = tk.Frame()
+  
+ #You can put things inside the frame by setting master option equal to frame
+  Labelx = tk.Label(frame1, text = "")
+  
 -Label  
   Label1 = tk.Label(root, text = "") #Can use entry.get() as a text
 
 -Button
-  Button1 = tk.Button(root, text = "", 
-                      command = )
+  Button1 = tk.Button(root, text = "")
+      command = buttonfunction  #Has the button do something when clicked
+      state = DISABLED    #Grays out and disables the button
+
+   #  Having the button do something. Make a function
+     def buttonfunction():
+        aLabel = tk.Label(root, text="do stuff")
+        aLabel.pack()
+  
 -Entry/Input
   entry1 = tk.Entry(root)
   entry1.get()    #stores what is typed into the entry widget
@@ -31,6 +47,15 @@ import tkinter as tk
   r.get()
   rb = tk.Radiobutton(root, text = "", variable = r, value = x)
   
+-Checkboxes
+ #Need variables to tied to them
+  v = IntVar()
+  
+  c = Checkbutton(root, text = "", variable = v)
+                 onvalue   #default value when box is checked
+                 offvalue    #default when box is unchecked
+  c.deselect()          #box is unchecked  by default
+
 -Message popup
   def popup():
      tk.messagebox.showinfo("Title of box", "The actual message") #Add this to a button as a command
@@ -42,10 +67,28 @@ import tkinter as tk
 -SpinBox
   sp = tk.Spinbox(root, from_ = x, to = y)  #x is minimum, y is maxium
   
+-Dropdown menu
+  clicked = tk.StringVar()      #change to IntVar() if using numbers
+  clicked.set()             #default option
+  
+  drop = tk.OptionMenu(root, clicked, "option1", "option2")
+  drop.pack()
+  
 -Browse files
   root.filename = tk.filedialog.askopenfilename(
     initialdir = "",
     title = "",
     filetypes = (("Text files", "*.txt*"), ("all files", "*.*"))) #can choose which files to search 
     
+-Menu
+  menubar = tk.Menu(root)
+  file = Menu(menubar, tearoff = 0)
+  menubar.add_cascade(label ='', menu = file)  #parent menu 
+  file.add_command(label ='', command = None)  #menu item
+  file.add_separator()   #adds horizontal line in menu
+  file.add_command(label ='', command = root.destroy)  #destorys the menu and selection
+      
+  # display Menu
+  root.config(menu = menubar)
+  
     
