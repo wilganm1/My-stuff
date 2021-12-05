c48e8b39743df0b958dae03a12477e9374ad4a15
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-12-04 20:33:48 -0500

    Update main.py

--- Python/Tkinter/Neural Network GUI/main.py
+++ Python/Tkinter/Neural Network GUI/main.py
@@ -4,79 +4,78 @@ represent nodes and weights. Also in the future try
 to get it to update in real-time during backwards
 propagation """
 
+""" I want to make a Neural Network GUI for myself to visually represent how they work.
+
+This will be small scale but I want to incorporate many things like being able to animate it
+using the time package, change colors of the canvas, input your own inputs with csvs or just typing, 
+and do complete backwards propagation. """
+
+
 from tkinter import *
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry('800x600')  #arbitrary numbers.
-
-canvas = tk.Canvas(master = root)
-        
-canvas_width = 800
-canvas_height = 600
-
-""" Getting the number of nodes from an entry widget"""
+root.geometry('1200x800')
 
 
-numb_nodes = IntVar()
-#get number using .get
-#numb_nodes.get
+frame1 = tk.Frame(root)
 
-""" Get locations of circles. More circles means
-they will get smaller. Need to be same size.
-Packing the canvas with .pack() always centers it,
-So that makes the math easier. I want the inputs 
-on the left-most side and outputs on right-most side.
-Right now I will not have any hidden layers. So this 
-means that based on pure aesthetics the layers will 
-be on the two halves of the screen.
-x coordinate of middle of circles always same
-middle of left half, so 1/4th of width of canvas
-y coordinate is dependent on how many circles there 
-are. Will always be number of nodes/2.
-    Example: 3 nodes = screen_height/6 """
-        
-radius_x = canvas_width/4
-radius_y = canvas_height/(numb_nodes.get()/2) 
-#!!! change y according to number of nodes selected 
+input_label = tk.Label(frame1,
+                       text = "Select number of inputs").grid(row=0, column = 0)
+input_select = tk.Spinbox(frame1, from_ = 2, to = 8, width = 3, wrap = True)
+input_select.grid(row = 1, column = 0)
 
-radius = radius_y/2
+output_label = tk.Label(frame1,
+                       text = "Select number of outputs").grid(row=0, column = 2)
+output_select = tk.Spinbox(frame1, from_ = 1, to = 6, width = 3, wrap = True)
+output_select.grid(row = 1, column = 2)                                                             
 
-#!!! If there were only two circles
-""" radius_y = canvas_height/2/2
-radius = radius_y/2 == canvas_height/8 """
+frame1.pack()
 
-x1 = radius_x - radius
-y1 = radius_y - radius
-x2 = radius_x + radius
-y2 = radius_y + radius
+frame2 = tk.Frame(root)
 
-# !!!!!!!!!!!!!!!!!!!
-''' Just realized something. How am I going to 
-consistently get the circles in the correct spots
-every single time. If I have two cirlces, and 
-the middle is 1/4th the screen height, how will that
-work with the node at the bottom?????
-Solution: Mulitply radius_y by the odd numbers 
-of the sections of the screen height.
-  Example: 2 nodes == 4 sections. 
-      sections = [1,2,3,4]
- I need to multiply the radius_y by the odd numbers
- in the sections list.
-  
-    1st circle radius_y = 1/4 screen height (1/4 * 1)
-    2nd cirlce radius_y = 3/4 screen height (1/4 * 3)
+mybutton = tk.Button(frame2, text = "Submit",
+                     command = create_canvas)
+mybutton.pack()
+frame2.pack()
+
+def create_canvas():
+    frame3 = tk.Frame(root)
     
-    list(range(1,(numb_nodes.get() * 2),2))
-for i in list(range(1,(numb_nodes.get() * 2),2)):
-    canvas.create_oval(x1, (radius_y * i) - radius, x2, (radius_y * i) + radius)
-'''
-
-
-for i in range(1,4):
-    canvas.create_oval(x1, i*y1 + i*y1, x2, (i*y1 + i*y1) + (x2-x1))
-canvas.pack(fill = tk.BOTH)
-
-
+    canvas_width = 1200
+    canvas_height = 600
+    canvas = tk.Canvas(frame3, width = canvas_width, height = canvas_height)
+    
+    dot_middle_x = canvas_width/4 #1/4th for inputs
+    output_middle = dot_middle_x * 3 #3/4ths for outputs
+    
+    radius = canvas_height/int(input_select.get())/4
+        
+    input_dot_middle_y = canvas_height/int(input_select.get())/2
+    output_dot_middle_y = canvas_height/int(output_select.get())/2
+    
+    
+    for i in range(1, int(input_select.get())*2 + 2, 2):
+        canvas.create_oval(dot_middle_x - radius,
+                           (input_dot_middle_y *i) - radius,
+                           dot_middle_x + radius,
+                           (input_dot_middle_y *i) + radius)
+    
+    for j in range(1, int(output_select.get())*2 + 2, 2):
+        canvas.create_oval(output_middle - radius,
+                           (output_dot_middle_y *j) - radius,
+                           output_middle + radius,
+                           (output_dot_middle_y *j) + radius)
+    
+    canvas.pack()
+    frame3.pack()
+    delete_Button()
+    
+def delete_Button():
+    frame4 = tk.Frame(root)
+    delete_button = tk.Button(frame4, text = "Reset", command = delete_canvas)
+    delete_button.pack()
+    frame4.pack()
+    
 
 root.mainloop()
