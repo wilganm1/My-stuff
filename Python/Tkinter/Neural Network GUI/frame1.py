85dd640b2af87d6684fe7c5d33c9c548a13500ab
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2021-12-11 17:07:29 -0500

    Update frame1.py

--- Python/Tkinter/Neural Network GUI/frame1.py
+++ Python/Tkinter/Neural Network GUI/frame1.py
@@ -1,18 +1,50 @@
-import tkinter as tk
-from tkinter import *
+number_of_inputs = 0
+the_inputs = 0
+inputs_range = 0
 
-root = tk.Tk()
+frame1 = tk.Frame(root)
 
-nodes_label = tk.Label(root, text='Select number of nodes')
-nodes_label.grid(row = 0, column = 1)
+def selected(event):
+    if clicked.get() == "CSV File":
+        while 1:
+            f = fd.askopenfile()
+            if f is None:
+                reply = tk.messagebox.askretrycancel(
+                'Error',
+                 "Please select a CSV File")
+                if reply == True:
+                    continue
+                else:
+                    break 
+            else:
+                csv_string = f.read()
+                csv_list = csv_string.split(",")
+                csv_map_object = map(int, csv_list)
+                csv_map = list(csv_map_object)
+    elif clicked.get() == 'Random':
+        global inputs_range
+        inputs_range = tk.simpledialog.askinteger(
+            "Input range",
+            'Enter range of 0 to X.',
+            parent = frame1,
+            minvalue=1)
+        global number_of_inputs
+        number_of_inputs = tk.simpledialog.askinteger(
+            "Input Amount",
+            "Enter amouont of inputs",
+            parent = frame1,
+            minvalue = 1)
+        global inputs
+        the_inputs = np.random.uniform(
+            0, inputs_range, number_of_inputs+1)
+        
+        
+options = ['CSV File', 'Direct Input', 'Random']
+clicked = tk.StringVar()
+clicked.set('')
 
+drop = tk.OptionMenu(root, clicked, *options,
+                     command = selected)
+drop.pack()
 
-nodes_entry1 = tk.Entry(width = 3) #entry widget
-
-nodes_entry2 = Spinbox(master, from_=0, to=10)  #change 10 to infinity
-nodes_entry2.grid(row = 1,column = 1
-
-#Or instead of a spinbox have it be an entry that checks if it's
-#greater than zero and can be converted to integer.
-
-root.mainloop()
+frame1.pack()
