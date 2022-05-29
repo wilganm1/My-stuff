ebc1aeedae129624e19ab8681e14a0fa5e52ada0
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2022-05-29 09:57:05 -0400

    Update React.js

--- Web dev/JavaScript/React.js
+++ Web dev/JavaScript/React.js
@@ -1,4 +1,10 @@
 -------------------------------BASICS-------------------------------
+How to create a new React app in the terminal
+ npx create-react-app my-app
+ cd my-app
+ npm start
+
+
 React is a JavaScript package used to create user interfaces.;
 
 React uses a JavaScript extension called JSX which combines HTML and JavaScript;
@@ -42,46 +48,45 @@ The DOM renders this div;
    root.render(element); //element is a javascript code that is passed;
 
 ---------------------------COMPONENTS & PROPS---------------------------;
-Components are pieces of the UI They are like functions that take in;
-They are created as a function take in "props" (short for;
+Components are pieces of the UI that act like functions.;
+They are created as a function and take in "props" (short for;
 properties) which are then rendered by the ReactDOM;
 
-  const Header = (props) => {
+An easy way to use props is by destructuring it with {} so you dont need;
+to use the word 'prop';
+
+  const Header = ({ title }) => {  //This uses the function shortcut () => {}
   return (
     <header>
-        <h1>{props.title}</h1>;
+        <h1>title</h1>;
     </header>)};
-    
+                 **************************
   The Header function is called by using <Header title = "~~";>
-    
-    
+                 **************************
   function App() {
     return (
       <div className="container">
-         <Header title = 'Hello' /> //the Header variables is called and the prop is set in the tag.
+         <Header title = {'Hello'} /> //the Header variables is called and the prop is set in the tag.
       </div>);};
-      
-Because functions are used they can be reused unlimited times.;
 
-  function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;}
+--------------------------------EVENTS & STATES--------------------------------
+States represent the current situation of a component.;
 
-  function App() {
-    return (
-      <div>
-        <Welcome name="Sara" /> 
-        <Welcome name="Cahal" />  
-        <Welcome name="Edite" />   
-      </div>);}
-
---------------------------------EVENT HANDLING--------------------------------
+  First import the useState object;
+  Then create an  array variable for the state;
+   
+   import {useState} from 'react';   //importing states
+  
+   const [stateName, setStateName] = useState();  //Setting up the State object
+//!!! Have the 2nd item in the array use the word 'set' and the name of the state.
+   
+   
 Handinling events is similar in standard HTML;
 A function is called when an HTML event happens;
      
      <button onClick={functionName}>  //
        Activate Lasers
      </button>
-
      
 
 
