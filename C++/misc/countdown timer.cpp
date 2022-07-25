8cf7595f0b43b55072c610a6d01f725d698c7ef8
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2022-07-25 13:15:42 -0400

    Update countdown timer.cpp

--- C++/misc/countdown timer.cpp
+++ C++/misc/countdown timer.cpp
@@ -1,3 +1,5 @@
+//                         Option 1. 
+
 #include <iostream>     // std::cout, std::endl
 #include <thread>       // std::this_thread::sleep_for
 #include <chrono>       // std::chrono::seconds
@@ -9,7 +11,16 @@ int main()
     std::cout << i << std::endl;
     std::this_thread::sleep_for (std::chrono::seconds(1)); //pauses for 1 second
   }
-  std::cout << "Lift off!\n";
+  return 0;}
 
-  return 0;
-}
+//                         Option 2.
+
+#include <iostream>
+#include <Windows.h> // Need this for Sleep(milliseconds) function
+
+int main() {
+    for (int i = 10; i > 0; i-- ){
+        std::cout << i << std::endl;
+        Sleep(1000); 
+        system("CLS");
+    }}
