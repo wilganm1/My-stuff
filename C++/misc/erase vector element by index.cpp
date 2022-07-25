0dad684f28475b605f2629fc0741d618d1cfdca1
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2022-07-25 19:51:40 -0400

    Update erase vector element by index.cpp

--- C++/misc/erase vector element by index.cpp
+++ C++/misc/erase vector element by index.cpp
@@ -4,15 +4,15 @@
 
 int main() 
 {
-    std::vector<int> v = { }; //Add numbers
+    std::vector<int> VECTORNAME = {     }; //Add numbers
     int key = ;   // CHANGE THIS TO WHAT YOU WANT TO FIND
 
-    std::vector<int>::iterator itr = std::find(v.begin(), v.end(), key); //iterates through vector
-    int index = std::distance(v.begin(), itr); 
+    std::vector<int>::iterator itr = std::find(VECTORNAME.begin(), VECTORNAME.end(), key); //iterates through vector
+    int index = std::distance(VECTORNAME.begin(), itr); 
     
     std::cout << index << std::endl; //finds index of key
-    std::cout << v[index] << std::endl;
-    v.erase(v.begin() + index);
+    std::cout << VECTORNAME[index] << std::endl;
+    v.erase(VECTORNAME.begin() + index);
 
 
     return 0;
