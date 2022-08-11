8efb12794b2cf210e4548e3be7c9ed363e3e26c5
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2022-08-11 17:14:25 -0400

    Update refresh tkinter canvas.py

--- Python/misc./refresh tkinter canvas.py
+++ Python/misc./refresh tkinter canvas.py
@@ -5,7 +5,8 @@ from tkinter import *  #import tkinter. You can import is as tk if you want.
 canvas = Canvas(width = ~~, height = ~~)  #Create your canvas widget but don't fill it.
 
 def refresh():  #make a function that will fill in the Canvas.
-  canvas.delete("all")   #.delete('all') will make an existing canvas completely clean. It keeps the widget.
+  canvas.delete("all")   #!!! .delete('all') will make an existing canvas completely clean. It keeps the widget.
+  
   """
   
   Enter canvas stuff here       This is where you add in your actual canvas.
@@ -15,5 +16,4 @@ def refresh():  #make a function that will fill in the Canvas.
   canvas.pack()      #This needs to be in the function.
 ''' Everytime you call the refresh function it will delete the previous canvas and add in the new one. The first time this function is called there is nothing to 
 delete, so the canvas will be filled in with whatever is in the funciton. But the next time the function is called is will delete the previous canvas and fill in 
-the new one. This happens everytime the function is called. '''  
-
+the new one. This happens everytime the function is called. Think of it like system("CLS") in C++ '''  
