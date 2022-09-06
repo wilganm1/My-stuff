b6a67a29c6f4cfdf20a9c8d289d0b0a97a1693d8
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2022-09-05 20:49:08 -0400

    Update Hangman.cpp

--- C++/Hangman.cpp
+++ C++/Hangman.cpp
@@ -16,10 +16,10 @@ void print(std::vector<char> const &lts){  //prints out available positions. fou
 
 void game(std::vector<char>& bp, std::vector<char>& the_letters, std::vector<char>& lg){
     system("CLS");
-    std::cout << "\t      _______ \t\t Letters Guessed"  << std::endl;
-    std::cout << "\t     |       |\t\t-----------------" << std::endl;
-    std::cout << "\t     "<<bp[0]<<"       |\t\t      "<< lg[0] <<" " << lg[1] << " "<< lg[2] << std::endl;
-    std::cout << "\t    "<<bp[2]<<bp[1]<<bp[3]<<"      |\t\t      " <<lg[3] << " " << lg[4] << " " << lg[5] << std::endl;
+    std::cout << "\t      _______ \t Letters Guessed"  << std::endl;
+    std::cout << "\t     |       |\t-----------------" << std::endl;
+    std::cout << "\t     "<<bp[0]<<"       |\t     "<< lg[0] <<" " << lg[1] << " "<< lg[2] << std::endl;
+    std::cout << "\t    "<<bp[2]<<bp[1]<<bp[3]<<"      |\t     " <<lg[3] << " " << lg[4] << " " << lg[5] << std::endl;
     std::cout << "\t    "<<bp[4]<< " "<<bp[5]<<"      |" << std::endl;
     std::cout << "\t           __|__\t \n" << std::endl;
 }
@@ -98,94 +98,102 @@ void rules(std::vector<char>& word, std::vector<char>& body_parts, std::vector<c
                 letters_left--;}}}
     if (wrong_guesses == 6){
         print(the_letters);
-        std::cout << "\n\n\t\t Out of guesses." << std::endl;}
+        std::cout << "\n\n\t\t Out of guesses." << std::endl;
+        print(word);}
     else if (letters_left == 0){
         print(the_letters);
         std::cout << "\n\n\t\t You got it!" << std::endl;}
 }
 
 int main(){
-    std::vector<char> word{};
-    std::string random_word();
-    std::vector<char> body_parts = {' ',' ',' ',' ',' ',' '};
-    std::vector<char> the_letters = {};
-    std::vector<char> letters_guessed = {' ',' ',' ',' ',' ',' '};
-    int word_length{};
     while (true){
-        std::cout << "Choose word length from 3 to 7: ";
-        std::cin >> word_length;
-        if (std::cin.fail()){ //will catch if goal is not an integer
-            std::cout << "\nNot an number. Try again." << std::endl;
-            std::cin.clear();  //will erase the cin input
-            std::cin.ignore(10000, '\n');}
-        else if (word_length < 3 || word_length > 7){
-            std::cout << "Out of range. Please try again." << std::endl;}
-        else {
-            for (int i = 0; i < word_length; i++){
-                the_letters.push_back('_');} //Corresponds to word length
-            break;}}
-    switch (word_length){   //making different arrays
-        case 3:{
-            std::vector<std::string> three_letter_words = {"owl","ant","sky","ski","dog","cat","ham","hit","hot","ran","car","van","box","all","ice","zoo","sly","mix","man","ape","ken","bee","sin","gag",
-            "gig","gym","fun","out","jew","few","fly","fat","pop","put","pin","pat","lye","sap","spy","cry","any","yam","bay","bar","dry","dab","ace","lol",
-            "kip","kin","wet","see","sea","eye","elk","elm","oak","red","vat","que","lab","row"};
-            int size_three_let = three_letter_words.size();
-            std::string random_word = three_letter_words[Random::get(0, (size_three_let-1))];
-            for (int i = 0; i < random_word.size(); i++){
-                word.push_back(static_cast<char>(random_word[i]));}
-            game(body_parts, the_letters,letters_guessed);
-            std::cout << "\n\n" << word[0] << word[1] << word[2];
-            rules(word,body_parts,the_letters,letters_guessed);
+        std::vector<char> word{};
+        std::string random_word();
+        std::vector<char> body_parts = {' ',' ',' ',' ',' ',' '};
+        std::vector<char> the_letters = {};
+        std::vector<char> letters_guessed = {' ',' ',' ',' ',' ',' '};
+        int word_length{};
+        while (true){
+            std::cout << "Choose word length from 3 to 7: ";
+            std::cin >> word_length;
+            if (std::cin.fail()){ //will catch if goal is not an integer
+                std::cout << "\nNot an number. Try again." << std::endl;
+                std::cin.clear();  //will erase the cin input
+                std::cin.ignore(10000, '\n');}
+            else if (word_length < 3 || word_length > 7){
+                std::cout << "Out of range. Please try again." << std::endl;}
+            else {
+                for (int i = 0; i < word_length; i++){
+                    the_letters.push_back('_');} //Corresponds to word length
+                break;}}
+        switch (word_length){   //making different arrays
+            case 3:{
+                std::vector<std::string> three_letter_words = {"owl","ant","sky","ski","dog","cat","ham","hit","hot","ran","car","van","box","all","ice","zoo","sly","mix","man","ape","ken","bee","sin","gag",
+                "gig","gym","fun","out","jew","few","fly","fat","pop","put","pin","pat","lye","sap","spy","cry","any","yam","bay","bar","dry","dab","ace","lol",
+                "kip","kin","wet","see","sea","eye","elk","elm","oak","red","vat","que","lab","row"};
+                int size_three_let = three_letter_words.size();
+                std::string random_word = three_letter_words[Random::get(0, (size_three_let-1))];
+                for (int i = 0; i < random_word.size(); i++){
+                    word.push_back(static_cast<char>(random_word[i]));}
+                game(body_parts, the_letters,letters_guessed);
+                rules(word,body_parts,the_letters,letters_guessed);
+                break;}
+            case 4:{
+                std::vector<std::string> four_letter_words = {"bark","crib","part","swim","swam","kiln","milk","pull", "dull","bull","past","last","long","song","pain","tear","tier","pier",
+                "made","blue","boon","boom","mask","scam","able","cope","dumb","yarn","yard","team","teal","beat","best","bets","bell","upon","lord","draw",
+                "bats","baby","hell","hike","hill","hale","none","noon","noun","nine","name","nail","rate","ream","read","rare","rung","raze","gaze","toss","boss","spin","spun",
+                "spur","joke","join","jest","only","over","once","pace","pies","ping","king","lawn","like","lame","dare","door","drip","vine","vain","barn","cats","crab",
+                "grab","drab","slab","guts","fire","tire","wire","lyre","sire","mire","idea","talk","dove","bard","tree","hard","fair","hair","hero","zero"};
+                int size_four_let = four_letter_words.size();
+                std::string random_word = four_letter_words[Random::get(0, (size_four_let-1))];
+                for (int i = 0; i < random_word.size(); i++){
+                    word.push_back(static_cast<char>(random_word[i]));}
+                game(body_parts,the_letters,letters_guessed);
+                rules(word,body_parts,the_letters,letters_guessed);
+                break;}
+            case 5:{
+                std::vector<std::string> five_letter_words = {"skull","skill","train","crane","swarm","swine","swing","bring","alarm","aloof","aloft","fable","fling","found","hound","curse","verse","arson",
+                "bread","breed","beard","joist","hoist","blast","crass","crude","drill","quill","value","olive","other","aroma","scent","sweet","piano","grant","lowly", "state","grate","great","wheel",
+                "while","first","third","fifth","sixth","tenth","bound","going","doing","devour","sleep","sweep","chill","drown","crown","slice","alive","mince","mouse","douse","fight","might","light",
+                "elder","elden","trust","crust","eight","ninth","square","money","hover","solve","digest","exodus","metal","blank","usual","rhyme","favor","awake","shout","coral"};
 
-            break;}
-        case 4:{
-            std::vector<std::string> four_letter_words = {"bark","crib","part","swim","swam","kiln","milk","pull", "dull","bull","past","last","long","song","pain","tear","tier","pier",
-            "made","blue","boon","boom","mask","scam","able","cope","dumb","yarn","yard","team","teal","beat","best","bets","bell","upon","lord","draw",
-            "bats","baby","hell","hike","hill","hale","none","noon","noun","nine","name","nail","rate","ream","read","rare","rung","raze","gaze","toss","boss","spin","spun",
-            "spur","joke","join","jest","only","over","once","pace","pies","ping","king","lawn","like","lame","dare","door","drip","vine","vain","barn","cats","crab",
-            "grab","drab","slab","guts","fire","tire","wire","lyre","sire","mire","idea","talk","dove","bard","tree","hard","fair","hair","hero","zero"};
-            int size_four_let = four_letter_words.size();
-            std::string random_word = four_letter_words[Random::get(0, (size_four_let-1))];
-            for (int i = 0; i < random_word.size(); i++){
-                word.push_back(static_cast<char>(random_word[i]));}
-            game(body_parts,the_letters,letters_guessed);
-            rules(word,body_parts,the_letters,letters_guessed);
-            break;}
-        case 5:{
-            std::vector<std::string> five_letter_words = {"skull","skill","train","crane","swarm","swine","swing","bring","alarm","aloof","aloft","fable","fling","found","hound","curse","verse","arson",
-            "bread","breed","beard","joist","hoist","blast","crass","crude","drill","quill","value","olive","other","aroma","scent","sweet","piano","grant","lowly", "state","grate","great","wheel",
-            "while","first","third","fifth","sixth","tenth","bound","going","doing","devour","sleep","sweep","chill","drown","crown","slice","alive","mince","mouse","douse","fight","might","light",
-            "elder","elden","trust","crust","eight","ninth","square","money","hover","solve","digest","exodus","metal","blank","usual","rhyme","favor","awake","shout","coral"};
+                int size_five_let = five_letter_words.size();
+                std::string random_word = five_letter_words[Random::get(0, (size_five_let-1))];
+                for (int i = 0; i < random_word.size(); i++){
+                    word.push_back(static_cast<char>(random_word[i]));}
+                game(body_parts,the_letters,letters_guessed);
+                rules(word,body_parts,the_letters,letters_guessed);
+                break;}
+            case 6:{
+                std::vector<std::string> six_letter_words = {"thrust","climax","rotate","fourth","silent","strong","monkey","reflex","oppose","detect","accept","abroad","actual",
+                "active","fusion","linear","breath","button","behind","caught","career","circle","effect","design","resign","export","little","nature","handed","people","strike","policy","prison","secret",
+                "screen","saying","starve","struck","spirit","source","useful","unless","unique","wealth","yellow","mellow","winter","fellow","endure","refine","prayer"};
 
-            int size_five_let = five_letter_words.size();
-            std::string random_word = five_letter_words[Random::get(0, (size_five_let-1))];
-            for (int i = 0; i < random_word.size(); i++){
-                word.push_back(static_cast<char>(random_word[i]));}
-            game(body_parts,the_letters,letters_guessed);
-            rules(word,body_parts,the_letters,letters_guessed);
+                int size_six_let = six_letter_words.size();
+                std::string random_word = six_letter_words[Random::get(0, (size_six_let-1))];
+                for (int i = 0; i < random_word.size(); i++){
+                    word.push_back(static_cast<char>(random_word[i]));}
+                game(body_parts,the_letters,letters_guessed);
+                rules(word,body_parts,the_letters,letters_guessed);
+                break;}
+            case 7:{
+                std::vector<std::string> seven_letter_words = {"slander","propose","entitle","dispose","stealth","remorse","villain","suggest","believe","discard","prevail","unravel","entwine",
+                "reverse","forsake","discard","requiem","request","survive","impress","genuine","jealous","revenge","conquer","execute","worship","enslave","prepare"};
+                int size_seven_let = seven_letter_words.size();
+                std::string random_word = seven_letter_words[Random::get(0, (size_seven_let-1))];
+                for (int i = 0; i < random_word.size(); i++){
+                    word.push_back(static_cast<char>(random_word[i]));}
+                game(body_parts,the_letters,letters_guessed);
+                rules(word,body_parts,the_letters,letters_guessed);
+                break;}
             break;}
-        case 6:{
-            std::vector<std::string> six_letter_words = {"thrust","climax","rotate","fourth","silent","strong","monkey","reflex","oppose","detect","accept","abroad","actual",
-            "active","fusion","linear","breath","button","behind","caught","career","circle","effect","design","resign","export","little","nature","handed","people","strike","policy","prison","secret",
-            "screen","saying","starve","struck","spirit","source","useful","unless","unique","wealth","yellow","mellow","winter","fellow","endure","refine","prayer"};
-
-            int size_six_let = six_letter_words.size();
-            std::string random_word = six_letter_words[Random::get(0, (size_six_let-1))];
-            for (int i = 0; i < random_word.size(); i++){
-                word.push_back(static_cast<char>(random_word[i]));}
-            game(body_parts,the_letters,letters_guessed);
-            rules(word,body_parts,the_letters,letters_guessed);
-            break;}
-        case 7:{
-            std::vector<std::string> seven_letter_words = {"slander","propose","entitle","dispose","stealth","remorse","villain","suggest","believe","discard","prevail","unravel","entwine",
-            "reverse","forsake","discard","requiem","request","survive","impress","genuine","jealous","revenge","conquer","execute","worship","enslave","prepare"};
-            int size_seven_let = seven_letter_words.size();
-            std::string random_word = seven_letter_words[Random::get(0, (size_seven_let-1))];
-            for (int i = 0; i < random_word.size(); i++){
-                word.push_back(static_cast<char>(random_word[i]));}
-            game(body_parts,the_letters,letters_guessed);
-            rules(word,body_parts,the_letters,letters_guessed);
-            break;}
-        break;}
+    char retry{};
+    std::cout << "\nPlay again?(y/n): ";
+    std::cin >> retry;
+    if (retry == 'y'){
+        system("CLS");}
+    else{
+        std::cout << "\nThanks for Playing!" << std::endl;
+        break;}}
     return 0;
 }
