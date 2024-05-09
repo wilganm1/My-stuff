a877cada38b04da8a272e3fc564b04737c7aaf39
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2024-05-09 14:48:29 -0400

    Create ai math equations and their derivatives.py

---
+++ Python/My AI/information/JSON text files/ai math equations and their derivatives.py
@@ -0,0 +1,17 @@
+{
+  "softplus": {
+    "equation": "log(1 + math.e**x)",
+    "derivative": "1 / (1 + math.e**-x)"},
+  "sigmoid": {
+    "equation": "(1 / (1 + math.e**-x) * (1 - (1 / (1 + math.e**-x))))"},
+  "tanh": {
+    "equation": "((2/ (1+ math.e**-2*x)) - 1)",
+    "derivative": "1 - ((2/ (1+ math.e**-2*x)) - 1)**2"},
+
+
+
+
+  
+}
+
+
