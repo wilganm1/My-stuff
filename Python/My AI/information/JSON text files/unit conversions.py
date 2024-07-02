4a2431101dc16137fe0f1d96574ec5490ab50464
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2024-07-02 15:26:07 -0400

    Update unit conversions.py

--- Python/My AI/information/JSON text files/unit conversions.py
+++ Python/My AI/information/JSON text files/unit conversions.py
@@ -1,1 +1,39 @@
-
+x = Measurments = {
+  "inch (in)": {
+    "inch (in)": 1,
+    "feet (ft)": 1/12,
+    "yard (yd)": 1/36,
+    "mile (mi)": 1.5783 * 10**-5 ,
+    "nautical mile ()": 1.3715 * 10**-5 ,
+    "nanometer (nm)": 2.54 * 10**7,
+    "micrometer (µm)": 25400,
+    "millimeter (mm)": 25.4,
+    "centimeter (cm)": 2.54,
+    "meter (m)": 0.0254,
+    "kilometer (km)": 2.54*10**-5},
+  
+  "feet (ft)": {
+    "inch (in)": 12,
+    "feet (ft)": 1,
+    "mile (mi)": 0.000189394,
+    "yard (yd)": 0.333333,
+    "nautical mile ()": 0.000164579,
+    "nanometer (nm)": 3.048*10**8,
+    "micrometer (µm)": 304800,
+    "millimeter (mm)": 304.8,
+    "centimeter (cm)": 30.48,
+    "meter (m)": 0.3048,
+    "kilometer (km)": 0.0003048},
+  
+  "mile (mi)": {
+    "inch (in)": 63360,
+    "feet (ft)": 5280,
+    "yard (yd)": 1760,
+    "nautical mile": ,
+    "nanometer (nm)": 1.609 *10**12,
+    "micrometer (µm)": 1.609 *10**9,
+    "millimeter (mm)": 1.609 *10**6,
+    "centimeter (cm)": 160934,
+    "meter (m)": 1609.34,
+    "kilometer (km)": 1.60934}, 
+}
