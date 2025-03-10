94cb15e1e8cbacfc9134b28b442bf1292b41e422
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-03-09 22:25:45 -0400

    Update Change wallpaper.cpp

--- C++/misc./Change wallpaper.cpp
+++ C++/misc./Change wallpaper.cpp
@@ -1,12 +1,15 @@
 #include <windows.h>
 #include <iostream>
-#include <conio.h>
+#include <chrono>
 
 int main () {
-  
-    const wchar_t *file = L"PATH TO PIC(S)";
-    getch();
-    int ret = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void *)file, SPIF_UPDATEINIFILE);
-    std::cout << ret << std::endl;
-    getch();
-}
+    const wchar_t* pics[] = {L"", L"", L""}; // <-- PATH TO PICS. Have to enter each one manually
+    int i;
+    while (1) {             //optional. Makes it infinite
+      for (i=0; i < 5; i++){
+          SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void *)pics[i], SPIF_UPDATEINIFILE);
+          Sleep(1000);  // in milliseconds.
+          };
+    }
+    return 0:
+};
