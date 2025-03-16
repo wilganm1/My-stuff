f887b40ab746023e2e49906662cf3899dbf20aca
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-03-15 20:04:51 -0400

    Create App.h

---
+++ C++/wxWidgets/Frames/App.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      [PROJECT NAME]App.h
+ * Purpose:   Defines Application Class
+ * Author:     ()
+ * Created:   2025-03-12
+ * Copyright:  ()
+ * License:
+ **************************************************************/
+
+#ifndef [PROJECT NAME]APP_H
+#define [PROJECT NAME]APP_H
+
+#include <wx/app.h>
+
+class [PROJECT NAME]App : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // [PROJECT NAME]APP_H
