a2d5d2c7b671f66f76b40ee4a466317d7f72c114
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-03-16 18:05:08 -0400

    Update Events.cpp

--- C++/wxWidgets/Events.cpp
+++ C++/wxWidgets/Events.cpp
@@ -12,7 +12,7 @@ Write this code in the mainframe.h file inside the mainframe class:
     void OnTextChanged(wxCommandEvent& evt);    // when text changes in box
     //Add whatever other events here
 
-Then back in the mainframe.cpp file write this code to define the command in the mainframe:
+Then back in the mainframe.cpp file write this code to define the event handler in the mainframe:
 
  void MainFrame::OnButtonClicked(wxCommandEvent& evt){
     wxLogStatus("Button Clicked");
@@ -29,4 +29,56 @@ Easy way to bind events to their handlers is to use the Bind() function
 
    button->Unbind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this); // Unbinds event
 
+
+---------------EVENT PROPOGATION----------------
+Event propogation is the idea that events are handled not by a specific control (button), but 
+by the panel/frame the control is in.
+
+
+                                 ___________________
+                                 |                 |
+                                 |      Frame      |
+                                 |_________________|
+                                          
+                                         /|\
+                                        / | \
+                                          |
+                     ____________________________________________
+                     |                                          |
+                     |                  Panel                   |
+                     |__________________________________________|
+                             /|\                         /|\
+                            / | \                       / | \
+                              |                           |
+                    ___________________          ___________________      
+                    |                 |          |                 |
+                    |      Button 1   |          |     Button 2    |
+                    |_________________|          |_________________|
+  
+
+ - All you have to do is use the this keyword when binding a control:
+
+      this->Bind(wxEVT_BUTTON, &MainFrame::OnButtonClicked, this);  // binds to panel
+
+- If you want separate events for each button, create their own event handles.
+
+              Header file
+     private:
+        void OnAnyButtonClicked(wxCommandEvent& evt);
+        void OnButton1Clicked(wxCommandEvent& evt);
+        void OnButton2Clicked(wxCommandEvent& evt);
+
+              .cpp file
+
+    void MainFrame::OnAnyButtonClicked(wxCommandEvent& evt){
+        wxLogStatus("Button Clicked");
+        evt.Skip();      <-- This skips the current-level binding and moves it up a level
+
+    void MainFrame::OnButton1Clicked(wxCommandEvent& evt){
+        wxLogStatus("Button Clicked");
+
+            now bind button1 to the event handle
+
+    button1->Bind(wxEVT_BUTTON, &MainFrame::OnButton1Clicked, this);
+    
   
