eeca0bf9c85908f322a59604e9adcf406ac64b85
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-03-15 20:37:33 -0400

    Update App.cpp

--- C++/wxWidgets/Frames/App.cpp
+++ C++/wxWidgets/Frames/App.cpp
@@ -1,30 +1,11 @@
-/***************************************************************
- * Name:      [PROJECT NAME]App.cpp
- * Purpose:   Code for Application Class
- * Author:     ()
- * Created:   2025-03-12
- * Copyright:  ()
- * License:
- **************************************************************/
+#include "App.h"
+#include "MainFrame.h"
+#include <wx/wx.h>
 
-#ifdef WX_PRECOMP
-#include "wx_pch.h"
-#endif
+wxIMPLEMENT_APP(App);
 
-#ifdef __BORLANDC__
-#pragma hdrstop
-#endif //__BORLANDC__
-
-#include "[PROJECT NAME]App.h"
-#include "[PROJECT NAME]Main.h"
-
-IMPLEMENT_APP([PROJECT NAME]App);
-
-bool [PROJECT NAME]App::OnInit()
-{
-    [PROJECT NAME]Frame* frame = new [PROJECT NAME]Frame(0L, _("wxWidgets Application Template"));
-    frame->SetIcon(wxICON(aaaa)); // To Set App Icon
-    frame->Show();
-    
+bool App::OnInit() {
+    MainFrame* mainFrame = new MainFrame("C++ GUI");
+    mainFrame->Show();
     return true;
 }
