558d349ce614e0bff8d6c74687ddb31e5dc2c9e7
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-03-22 15:53:34 -0400

    Update MainFrame.cpp

--- C++/wxWidgets/MainFrame.cpp
+++ C++/wxWidgets/MainFrame.cpp
@@ -1,21 +1,11 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
-enum IDs {       // Enum for IDs of controls
-    BUTTON_ID = 2
-};
-
-wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)                
-    EVT_BUTTON(BUTTON_ID, MainFrame::OnButtonClicked)   // Event table to process events
-wxEND_EVENT_TABLE()
 
 MainFrame::MainFrame(const wxString& title): wxFrame(nullptr, wxID_ANY, title){
-    wxPanel* panel = new wxPanel(this);
 
-    wxButton* button = new wxButton(panel, BUTTON_ID, "Tit", wxPoint(100,200), wxDefaultSize);
-
-    CreateStatusBar();
-
+    // Insert controls here
+    
 }
 
 void MainFrame::OnButtonClicked(wxCommandEvent& evt){    // function to display text on button click
