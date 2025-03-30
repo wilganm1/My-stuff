ca147382f7d8c1ff94f63ff8c8dae8d903162c23
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-03-29 22:48:37 -0400

    Update tips.cpp

--- C++/wxWidgets/tips.cpp
+++ C++/wxWidgets/tips.cpp
@@ -1,9 +1,15 @@
 ---------------------------------SIZERS---------------------------------
 
-Sizers control how panels and contrls act when dimensions of the window changes
+Sizers control how panels and buttons, etc. act when dimensions of the window changes
 
-When adding mutliple panels to a 2nd sizer you need to have a proportion and wxEXPAND to the panels AND 
-to the second sizer
+Panels must be associated with the frame then added to a sizer later.
+
+To have multiple sizers in one frame add a sizer to another sizer
+
+                        sizer1->add(sizer2, 1, wxEXPAND);
+
+When adding mutliple panels to a 2nd sizer you need to have a proportion and wxEXPAND to 
+the panels AND to the second sizer
 
             wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 
@@ -19,5 +25,13 @@ to the second sizer
 
   CANNOT DO this->SetSizerAndFit() TO MULTIPLE SIZERS, JUST ONE. ANY ADDITIONAL SIZERS MUST BE 
     ADDED TO ANOTHER SIZER.
-        
+
+When putting a panel inside another panel, first define nested panel as a child of its parent panel
+                                            |
+                                            v
+            wxPanel* panel3 = new wxPanel(panel2, wxID_ANY, wxDefaultPosition,
+                                            ^
+                                            |
+
+            
 
