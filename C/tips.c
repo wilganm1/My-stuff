bb234a4880c5131599ae2732d83f9efb16e2ddc3
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2025-04-22 11:27:20 -0400

    Update tips.c

--- C/tips.c
+++ C/tips.c
@@ -12,7 +12,7 @@ To call them simply type their name.
 The data type of a function depends on it's return type. If it returns nothing
 the data type is void.
   
- Variables can be defined within a function and belong only to it.
+ Variables can be defined within a function and belong only to it (local scope)
 
        void calculateSum() {
           int x = 5;
@@ -30,3 +30,50 @@ Their type and name must be declared when the function is made;
       }
 
       calculateSum(400,20);
+
+- Recursion
+                int sum(int k);
+
+                int main() {
+                  int result = sum(10);
+                  printf("%d", result);
+                  return 0;
+                }
+                
+                int sum(int k) {
+                  if (k > 0) {
+                    return k + sum(k - 1);
+                  } else {
+                    return 0;
+                  }
+                }
+#include <math.h>
+
+sqrt()        -square root
+ceil()        -round up
+floor()       -round down
+pow(x,y)      -raises x to power of y
+https://www.w3schools.com/c/c_ref_math.php
+
+
+----------------------------------FILES----------------------------------
+In C, you can create, open, read, and write to files by declaring a 
+pointer of type FILE, and use the fopen() function:
+
+
+        FILE *fptr;
+        fptr = fopen(filename, mode);
+                               -w    -creates a new file
+                               -a    -appends new data to file
+                               -r    -reads file
+        fclose(fptr)     -closes file
+
+If you want to create the file in a specific folder, just provide an absolute path (\)
+
+        fptr = fopen("C:\\directoryname\\filename.txt", "w");
+
+
+
+
+
+
