eb59165efc079ea4a2fc2f14718e17ef1e19d929
wilganm1 <68622072+wilganm1@users.noreply.github.com>
2026-01-26 13:18:49 -0500

    Create simple object detection.py

---
+++ Python/misc./simple object detection.py
@@ -0,0 +1,24 @@
+# github repository for YOLO model
+# https://github.com/codershiyar/object-detection-using-webcam
+
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('model.pt')
+print(model.names)
+webcamera = cv2.VideoCapture(0)
+# webcamera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+# webcamera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+while True:
+    success, frame = webcamera.read()
+    
+    results = model.track(frame, classes=0, conf=0.8, imgsz=480)
+    cv2.putText(frame, f"Total: {len(results[0].boxes)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.imshow("Live Camera", results[0].plot())
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+webcamera.release()
+cv2.destroyAllWindows()
